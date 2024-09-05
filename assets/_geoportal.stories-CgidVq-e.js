d9cf0b9e9ef9b21a0c366e0f70b25e65cc9dc3e8
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2024-09-05 09:56:53 +0000

    deploy: a79b5f39bb6e4ee2c76598a69dbea6e53e0ce462

---
+++ assets/_geoportal.stories-CgidVq-e.js
@@ -0,0 +1,111 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{G as u,I as a,D as Me,T as Ie}from"./TopicMapContextProvider-ZSrruAcQ.js";import{r as d}from"./index-RYns6xqu.js";import{I as He,F as Pe,n as Te,G as ze,M as De}from"./InKartePositionierenText-CNr5jwnF.js";import{G as Ne}from"./ModalApplicationMenu-Y3vw-nyv.js";import"./index-DNUR7M9R.js";import"./index-DvoZgHAm.js";import"./_baseClone-BMRAXyjt.js";import"./_getPrototype-Dwr3C2Zh.js";const be=()=>e.jsx("span",{children:"Could now she how in was sleep dropped in by back shall come into needed wild that picture they to have would by her succeeding."});be.__docgenInfo={description:"",methods:[],displayName:"Help05Introduction"};const Se=()=>e.jsx(u,{sectionKey:"datengrundlage",sectionTitle:"Datengrundlagen",sectionBsStyle:"warning",sectionContent:e.jsx("div",{children:"Could now she how in was sleep dropped in by back shall come into needed wild that picture they to have would by her succeeding, hair a with the I first should expected line of so got her solitary the and that coffee that omens, treble-range are quitting for high line text, parts else. A have the unprofitable both the leather at neighbours in on each written and assignment. When me the his degree read. She that the equation, of by hand. Select picture spare not got in social and parameters absolutely he and that if the alphabet carpeting of."})});Se.__docgenInfo={description:"",methods:[],displayName:"Component"};const ye=()=>e.jsx(u,{sectionKey:"positionieren",sectionTitle:"In Karte positionieren",sectionBsStyle:"info",sectionContent:e.jsx("div",{children:e.jsx(He,{pretext:e.jsx(e.Fragment,{children:e.jsxs("p",{children:["Um eine bestimmte Stelle des Stadtgebietes zu erkunden, geben Sie den Anfang eines Stadtteils (Stadtbezirk oder Quartier), einer Adresse, eines Straßennamens oder eines POI im Eingabefeld links unten ein (mindestens 2 Zeichen). In der inkrementellen Auswahlliste werden Ihnen passende Treffer angeboten. (Wenn Sie weitere Zeichen eingeben, wird der Inhalt der Auswahlliste angepasst.) Durch das vorangestellte Symbol erkennen Sie, ob es sich dabei um einen ",e.jsx(a,{name:"circle"})," ","Stadtbezirk, ein ",e.jsx(a,{name:"pie-chart"})," Quartier, eine"," ",e.jsx(a,{name:"home"})," Adresse, eine ",e.jsx(a,{name:"road"})," Straße ohne zugeordnete Hausnummern, einen ",e.jsx(a,{name:"tag"})," POI, die ",e.jsx(a,{name:"tags"})," alternative Bezeichnung eines POI, eine ",e.jsx(a,{name:"child"})," Kindertageseinrichtung oder eine"," ",e.jsx(a,{name:"graduation-cap"})," Schule handelt."]})})})})});ye.__docgenInfo={description:"",methods:[],displayName:"Component"};const Ae=""+new URL("MeinStandpunktMarkerMitKompass-CjzOs80a.jpg",import.meta.url).href,Oe=""+new URL("MeinStandpunktMarkerDoppel-63B_eDGU.jpg",import.meta.url).href,ve=()=>e.jsx(u,{sectionKey:"standort",sectionTitle:"Mein Standort",sectionBsStyle:"success",sectionContent:e.jsxs("div",{children:[e.jsxs("p",{children:['Das Werkzeug "Mein Standort"'," ",e.jsx(Pe,{icon:Te})," funktioniert so wie hier beschrieben nur auf mobilen Endgeräten wie Smartphones und Tablet-PC's, die i. d. R. mit einem GNSS-Empfänger als Positionssensor ausgestattet sind (GNSS steht für Global Navigation Satellite Systems, z.B."," ",e.jsx("a",{target:"_info",href:"https://dvw.de/sites/default/files/landesverein-bayern/VeroeffentlichungenMitteilungen1996_4/DVW_1996_4_Schlie%C3%9Fer_GPS_Systemgrundlagen.pdf",children:"GPS"}),'). Mit dem ersten Antippen dieses Werkzeugs aktivieren Sie den "Follow me"-Modus, erkennbar an der orangefarbigen Darstellung des Werkzeug-Symbols. Ggf. werden Sie vom Browser bzw. dem Betriebssystem Ihres Endgerätes zur Freigabe der Lokalisierung aufgefordert. Die Karte wird auf Ihren aktuellen Standort zentriert, der mit einem blauen Kreissymbol'," ",e.jsx("img",{alt:"MeinStandpunktMarker",src:Ae})," ","markiert wird. Das kleine blaue Dreieck am Rand dieses Standortsymbols zeigt Ihre aktuelle Blickrichtung an (genauer: die Richtung, in die Ihr Endgerät zeigt). Das Standortsymbol ist umgeben von einem zweiten Kreis mit transparenter, blauer Füllung, dessen Radius die Unsicherheit der Positionsbestimmung angibt"," ",e.jsx("img",{alt:"MeinStandpunktMarkerDoppel",src:Oe}),'. Durch Antippen des inneren Kreises können Sie einen Texthinweis mit einer konkreten Angabe der aktuellen Positionsgenauigkeit einblenden. Dieser Hinweis kann durch das Antippen einer beliebigen Stelle außerhalb seines Textfeldes geschlossen werden. Wenn Sie sich mit Ihrem Endgerät bewegen (z. B. bei einer Wanderung), wird Ihre aktuelle Position im "Follow me"-Modus in schneller Folge neu bestimmt und stets in der Mitte der Karte angezeigt. Die Karte wird dazu automatisiert nachgeführt. Ein weiteres Antippen von "Mein Standort" schaltet die Anzeige Ihrer Position wieder ab (schwarzes Werkzeug-Symbol).'," "]}),e.jsxs("p",{children:['Wenn Sie die Karte im aktivierten "Follow me"-Modus manuell verschieben, wird der "Follow me"-Modus unterbrochen, erkennbar an einer blauen Darstellung des Werkzeug-Symbols. Ihr aktueller Standort wird weiterhin angezeigt und in schneller Folge aktualisiert. Der Kartenausschnitt wird jetzt aber nicht mehr automatisch an den aktuellen Standort angepasst. Bei fortgesetzter Bewegung in eine Richtung wird das Standortsymbol daher vom Zentrum zum Rand des Kartenfensters wandern und dann verschwinden - Ihr Standort liegt jetzt nicht mehr im aktuell eingestellten Kartenausschnitt! In diesem Fall führt ein erneutes Antippen von "Mein Standort" dazu, dass wieder in den "Follow me"-Modus zurückgewechselt wird. Wenn Ihr aktueller Standort dagegen noch in der Karte sichtbar ist, wird durch diese Aktion die Anzeige der Position abgeschaltet (schwarzes Werkzeug-Symbol). Sie können die Unterbrechung des "Follow me"-Modus bei einer Wanderung o. ä. nutzen, wenn Sie sich die vor Ihnen liegende Strecke in der Karte anschauen wollen. Verschieben Sie dazu den Kartenausschnitt manuell auf den Abschnitt Ihres Interesses. Ihr aktueller Standort wird dann zumeist nicht mehr dargestellt werden können. Der "Follow me"-Modus wird unterbrochen (blaues Werkzeug-Symbol). Wenn Sie sich ausreichend über den weiteren Streckenverlauf informiert haben, reaktivieren Sie durch erneutes Antippen von "Mein Standort" den "Follow me"-Modus und setzen die Karte damit wieder auf Ihren aktuellen Standort zurück.'," "]}),e.jsx("p",{children:"Noch ein Hinweis zur Genauigkeit: Die Richtigkeit der Positionsanzeige ist nicht garantiert. Ihre Genauigkeit hängt von verschiedenen Faktoren ab, u. a. von der Qualität des GNSS-Empfängers in Ihrem Endgerät, der Umgebungssituation (z. B. hohe Gebäude oder spiegelnde Oberflächen in der Nachbarschaft) sowie der Methode, mit der Ihr Endgerät und der von Ihnen verwendete Browser die Position bestimmen."})]})});ve.__docgenInfo={description:"",methods:[],displayName:"Component"};const ke=()=>e.jsx(u,{sectionKey:"haftungsausschluss",sectionTitle:"Haftungsausschluss",sectionBsStyle:"danger",sectionContent:e.jsx("div",{children:"Could now she how in was sleep dropped in by back shall come into needed wild that picture they to have would by her succeeding, hair a with the I first should expected line of so got her solitary the and that coffee that omens, treble-range are quitting for high line text, parts else. A have the unprofitable both the leather at neighbours in on each written and assignment. When me the his degree read. She that the equation, of by hand. Select picture spare not got in social and parameters absolutely he and that if the alphabet carpeting of."})});ke.__docgenInfo={description:"",methods:[],displayName:"Component"};const xe=()=>e.jsx(u,{sectionKey:"zwilling",sectionTitle:"Urbaner Digitaler Zwilling",sectionBsStyle:"warning",sectionContent:e.jsx(ze,{})});xe.__docgenInfo={description:"",methods:[],displayName:"Component"};const we=({version:t,setAppMenuActiveMenuSection:n})=>e.jsx(De,{title:"Geoportal",version:t,setAppMenuActiveMenuSection:n});we.__docgenInfo={description:"",methods:[],displayName:"Footer",props:{version:{required:!0,tsType:{name:"string"},description:""},setAppMenuActiveMenuSection:{required:!0,tsType:{name:"signature",type:"function",raw:"(arg: string) => void",signature:{arguments:[{type:{name:"string"},name:"arg"}],return:{name:"void"}}},description:""}}};const _e="Kompaktanleitung und Hintergrundinformationen",Ge="Wohin?",o={menu:{key:"MENULEISTE",containerPos:"center",contentPos:"center",content:e.jsx("div",{children:"Menüleiste"})},layerWrapper:{key:"LAYERBUTTONS",containerPos:"center",contentPos:"center",content:e.jsx("div",{children:"Layer Buttons"})},hintergrund:{key:"HINTERGRUND",content:e.jsx("div",{children:"Hintergrund"}),containerPos:"center",contentPos:"center"},modalMenu:{key:"MENU",containerPos:"center",contentPos:"center",content:e.jsx("div",{children:"Menü"})},zoomControl:{key:"ZOOM",content:e.jsx("div",{children:"Zoom"}),containerPos:"center",contentPos:"left-center"},fullScreenControl:{key:"VOLLBILD",content:e.jsx("div",{children:"Vollbild"}),containerPos:"center",contentPos:"left-center"},navigatorControl:{key:"MEINE_POSITION",content:e.jsx("div",{children:"Meine Position"}),containerPos:"center",contentPos:"left-center"},homeControl:{key:"RATHAUS",content:e.jsx("div",{children:"Rathaus"}),containerPos:"center",contentPos:"left-center"},measurementControl:{key:"MESSUNGEN",content:e.jsx("div",{children:"Messungen"}),containerPos:"center",contentPos:"left-center"},gazetteerControl:{key:"GAZETTEER_SUCHE",content:"Gazetteer Suche",containerPos:"center",contentPos:"center"}},Ke={transparency:.8,color:"#000000"},Re=({versionString:t})=>{const n=()=>{const{setAppMenuActiveMenuSection:g}=d.useContext(Me);return e.jsx(we,{version:t,setAppMenuActiveMenuSection:g})};return{menuIntroduction:e.jsx(be,{}),menuIcon:"info",menuTitle:"Kompaktanleitung und Hintergrundinformationen",menuSections:[e.jsx(Se,{},"Datengrundlage"),e.jsx(ye,{},"InKartePositionieren"),e.jsx(ve,{},"MeinStandort"),e.jsx(ke,{},"Haftungsausschluss"),e.jsx(xe,{},"zwilling")],menuFooter:e.jsx(n,{})}},s=(t,n)=>{for(const c in n)if(n[c].key===t)return{primary:{...n[c]}}};function r({config:t,transparency:n=.8,color:c="white"}){const[l,m]=d.useState(null);return d.useEffect(()=>{if(t){const{content:p,containerPos:g,contentPos:je}=t,Ce=Ue(g),Ee=Be(je);m({pos:Ce,contPos:Ee,content:p})}},[t]),e.jsx("div",{style:{position:"fixed",top:0,zIndex:1e3,width:"100vw",height:"100vh",background:c,opacity:n},children:e.jsx("div",{style:{marginTop:"1rem"},children:e.jsx("span",{children:l?l.content:""})})})}function Ue(t){let n={};switch(t){case"center":n.transform="translate(0, 0)";break;case"top":n.transform="translate(0, -100%)";break;case"left":n.transform="translate(-100%, 0)";break;case"right":n.transform="translate(100%, 0)";break;case"bottom":n.transform="translate(0, 100%)";break;default:console.log("yyy element position")}return n}function Be(t){let n={};switch(t){case"center":n.top="50%",n.left="50%",n.transform="translate(-50%, -50%)";break;case"top-center":n.top=0,n.transform="translate(50%, 0)";break;case"top-right":n.top=0,n.right=0;break;case"top-left":n.top=0,n.left=0;break;case"left-center":n.top="50%",n.transform="translate(0, -50%)",n.left=0;break;case"left-top":n.top=0,n.left=0;break;case"left-bottom":n.bottom=0,n.left=0;break;case"right-center":n.top="50%",n.transform="translate(0, -50%)",n.right=0;break;case"right-top":n.top=0,n.right=0;break;case"right-bottom":n.bottom=0,n.right=0;break;case"bottom-center":n.bottom=0,n.transform="translate(50%, 0)";break;case"bottom-right":n.bottom=0,n.right=0;break;case"bottom-left":n.bottom=0,n.left=0;break;default:console.log("yyy content position")}return n}r.__docgenInfo={description:"",methods:[],displayName:"LibHelperOverlay",props:{transparency:{defaultValue:{value:"0.8",computed:!1},required:!1},color:{defaultValue:{value:'"white"',computed:!1},required:!1}}};const i=(t={primary:{containerPos:"center",contentPos:"center",content:e.jsx("div",{})}})=>{const[n,c]=d.useState();return d.useLayoutEffect(()=>{if(t!=null&&t.primary&&!n){const{containerPos:l,contentPos:m,content:p}=t.primary;c({content:p,containerPos:l,contentPos:m})}},[t,n]),n},Je={title:"Geoportal"},h={render:()=>e.jsx(Ie,{appKey:"storybook-appkey",children:e.jsx(Ne,{...Re({versionString:"xyz"}),visible:!0})})},f={render:()=>e.jsx("div",{children:Ge})},b={render:()=>e.jsx("div",{children:_e})},S={render:()=>{const t=s("MENULEISTE",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},y={render:()=>{const t=s("LAYERBUTTONS",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},v={render:()=>{const t=s("HINTERGRUND",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},k={render:()=>{const t=s("MENU",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},x={render:()=>{const t=s("ZOOM",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},w={render:()=>{const t=s("VOLLBILD",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},j={render:()=>{const t=s("MEINE_POSITION",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},C={render:()=>{const t=s("RATHAUS",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},E={render:()=>{const t=s("MESSUNGEN",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},M={render:()=>{const t=s("GAZETTEER_SUCHE",o),n=i(t);return e.jsx("div",{children:e.jsx(r,{config:n})})}},I={render:()=>{const{transparency:t,color:n}=Ke;return e.jsx("div",{style:{width:"100%",height:"90vh",backgroundColor:n,opacity:t}})}};var H,P,T;h.parameters={...h.parameters,docs:{...(H=h.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: () => {
+    const versionString = "xyz";
+    return <TopicMapContextProvider appKey="storybook-appkey">
+        <ModalApplicationMenu {...getCollabedHelpComponentConfig({
+        versionString
+      })} visible={true} />
+      </TopicMapContextProvider>;
+  }
+}`,...(T=(P=h.parameters)==null?void 0:P.docs)==null?void 0:T.source}}};var z,D,N;f.parameters={...f.parameters,docs:{...(z=f.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{searchTextPlaceholder}</div>;
+  }
+}`,...(N=(D=f.parameters)==null?void 0:D.docs)==null?void 0:N.source}}};var A,O,_;b.parameters={...b.parameters,docs:{...(A=b.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{tooltipText}</div>;
+  }
+}`,...(_=(O=b.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var G,K,R;S.parameters={...S.parameters,docs:{...(G=S.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("MENULEISTE", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(R=(K=S.parameters)==null?void 0:K.docs)==null?void 0:R.source}}};var U,B,L;y.parameters={...y.parameters,docs:{...(U=y.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("LAYERBUTTONS", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(L=(B=y.parameters)==null?void 0:B.docs)==null?void 0:L.source}}};var F,W,Z;v.parameters={...v.parameters,docs:{...(F=v.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("HINTERGRUND", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(Z=(W=v.parameters)==null?void 0:W.docs)==null?void 0:Z.source}}};var V,q,$;k.parameters={...k.parameters,docs:{...(V=k.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("MENU", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...($=(q=k.parameters)==null?void 0:q.docs)==null?void 0:$.source}}};var Q,Y,J;x.parameters={...x.parameters,docs:{...(Q=x.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("ZOOM", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(J=(Y=x.parameters)==null?void 0:Y.docs)==null?void 0:J.source}}};var X,ee,ne;w.parameters={...w.parameters,docs:{...(X=w.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("VOLLBILD", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(ne=(ee=w.parameters)==null?void 0:ee.docs)==null?void 0:ne.source}}};var te,re,oe;j.parameters={...j.parameters,docs:{...(te=j.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("MEINE_POSITION", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(oe=(re=j.parameters)==null?void 0:re.docs)==null?void 0:oe.source}}};var se,ie,ae;C.parameters={...C.parameters,docs:{...(se=C.parameters)==null?void 0:se.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("RATHAUS", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(ae=(ie=C.parameters)==null?void 0:ie.docs)==null?void 0:ae.source}}};var ce,le,de;E.parameters={...E.parameters,docs:{...(ce=E.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("MESSUNGEN", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(de=(le=E.parameters)==null?void 0:le.docs)==null?void 0:de.source}}};var ue,me,pe;M.parameters={...M.parameters,docs:{...(ue=M.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  render: () => {
+    const element = getCollabedHelpElementsConfig("GAZETTEER_SUCHE", geoElements);
+    const config = useOverlayHelper(element);
+    return <div>
+        <DisplayContentHelper config={config} />
+      </div>;
+  }
+}`,...(pe=(me=M.parameters)==null?void 0:me.docs)==null?void 0:pe.source}}};var ge,he,fe;I.parameters={...I.parameters,docs:{...(ge=I.parameters)==null?void 0:ge.docs,source:{originalSource:`{
+  render: () => {
+    const {
+      transparency,
+      color
+    } = backgroundSettings;
+    return <div style={{
+      width: "100%",
+      height: "90vh",
+      backgroundColor: color,
+      opacity: transparency
+    }}></div>;
+  }
+}`,...(fe=(he=I.parameters)==null?void 0:he.docs)==null?void 0:fe.source}}};const Xe=["KompletterModalerDialog","TextInSearchPlaceholder","TextTooltip","Menüleiste","LayerButtons","Hintergrund","Menü","Zoom","Vollbild","MeinePosition","Rathaus","Messungen","GazetteerSuche","Background"];export{I as Background,M as GazetteerSuche,v as Hintergrund,h as KompletterModalerDialog,y as LayerButtons,j as MeinePosition,k as Menü,S as Menüleiste,E as Messungen,C as Rathaus,f as TextInSearchPlaceholder,b as TextTooltip,w as Vollbild,x as Zoom,Xe as __namedExportsOrder,Je as default};
