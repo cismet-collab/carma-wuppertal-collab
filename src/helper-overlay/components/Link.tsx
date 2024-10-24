ff8a4b93928a875be1925c86995c3e47948f3302
David Glogaza <david.glogaza40@googlemail.com>
2024-10-24 16:34:16 +0200

    add link to menu

--- src/helper-overlay/components/Link.tsx
+++ src/helper-overlay/components/Link.tsx
@@ -2,6 +2,7 @@ import { OverlayTourContext } from "./OverlayTourProvider";
 import type { OverlayTourContext as OverlayTourContextSettings } from "../helperTypes";
 import { useContext } from "react";
 import { UIDispatchContext } from "react-cismap/contexts/UIContextProvider";
+import { UIContext } from "react-cismap/contexts/UIContextProvider";
 
 interface LinkProps {
   children: JSX.Element | string;
@@ -10,14 +11,19 @@ interface LinkProps {
 }
 
 export const Link = ({ children, target, section }: LinkProps) => {
-  const { setSecondaryWithKey } =
+  const { setSecondaryWithKey, showOverlay } =
     useContext<OverlayTourContextSettings>(OverlayTourContext);
   const { setAppMenuActiveMenuSection, setAppMenuVisible } =
     useContext<typeof UIDispatchContext>(UIDispatchContext);
+  const { appMenuVisible } = useContext<typeof UIContext>(UIContext);
 
   const showSecondaryWithKeyHandler = (e) => {
     e.stopPropagation();
     if (target) {
+      if (appMenuVisible) {
+        setAppMenuVisible(false);
+        showOverlay(true);
+      }
       setSecondaryWithKey(target);
     }
 
