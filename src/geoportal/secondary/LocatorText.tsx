3c7be900fe04e79d28876bcb7a0574df70f19d98
default <stefan.sander@t-online.de>
2024-10-25 04:40:13 +0000

    Ausformuliertes Secondary für Locator

--- src/geoportal/secondary/LocatorText.tsx
+++ src/geoportal/secondary/LocatorText.tsx
@@ -7,8 +7,11 @@ export const LocatorText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        2Do: Text im Speisekartenstil mit <Link section="standort">Link</Link>{" "}
-        auf Kompaktanleitung
+        Wechselschalter für Aktivierung/Deaktivierung von Standort- und Blickrichtungsanzeige (<b>2D</b>, <b>mobil</b>)| 
+        Durchmesser des Standortsymbols visualisiert Unsicherheit der Positionsbestimmung | 
+        "Follow me"-Modus (Symbol auf Schaltfläche orange): Standortsymbol zentriert, automatische Kartennachführung | 
+        Verschiebung der Karte bewirkt Umstellung auf bewegliche Positionsanzeige (blaues Symbol)<br/> 
+        <Link section="standort">weitere Informationen</Link>{" "}
       </div>
     </ul>
   );
