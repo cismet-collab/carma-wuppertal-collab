98bffbef6cfe55e3ce9f59b676f2c3e7d7cf7aba
default <stefan.sander@t-online.de>
2024-11-26 06:26:39 +0000

    Secondary für Kartensteuerelemente V1

--- src/geoportal/secondary/LayerButtonsText.tsx
+++ src/geoportal/secondary/LayerButtonsText.tsx
@@ -5,7 +5,11 @@ export const LayerButtonsText = () => {
         style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
       >
         <div>
-          2Do  
+          Festes Hintergrund-Steuerelement wird immer angezeigt, dazu variabel 
+          für jede weitere geladene Kartenebene ein Ebenen-Steuerelement (<b>2D</b>) | 
+          horizontales Verschieben der Ebenen-Steuerelemente zur Änderung der Darstellungsreihenfolge | 
+          Klicken/Tippen auf das Steuerelement zum Öffnen einer Zeile für die Steuerung von 
+          Transparenz und Sichtbarkeit | Aufklappen der Zeile zur Anzeige weiterer Informationen  
         </div>
       </ul>
     );
