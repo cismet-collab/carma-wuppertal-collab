6441e97f862a334b02a911e03bd4c8a1068a1644
default <stefan.sander@t-online.de>
2024-11-26 07:57:12 +0000

    Secondary für Kartensteuerelemente V2

--- src/geoportal/secondary/LayerButtonsText.tsx
+++ src/geoportal/secondary/LayerButtonsText.tsx
@@ -1,3 +1,5 @@
+import { Link } from "../../helper-overlay/components/Link";
+
 export const LayerButtonsText = () => {
     return (
       <ul
@@ -9,8 +11,17 @@ export const LayerButtonsText = () => {
           für jede weitere geladene Kartenebene ein Ebenen-Steuerelement (<b>2D</b>) | 
           horizontales Verschieben der Ebenen-Steuerelemente zur Änderung der Darstellungsreihenfolge | 
           Klicken/Tippen auf das Steuerelement zum Öffnen einer Zeile für die Steuerung von 
-          Transparenz und Sichtbarkeit | Aufklappen der Zeile zur Anzeige weiterer Informationen  
+          Transparenz und Sichtbarkeit <br /><br /> 
+          Aufklappen der Zeile zur Anzeige weiterer Informationen:
         </div>
+        <li>
+          <b>Hintergrund-Steuerelement</b>: Auswahl/Voreinstellung der <Link target="HINTERGRUND">Hintergrundkarte</Link> | 
+          Informationen zur ausgewählten Hintergrundkarte | "Stapel" mit den Bedienungszeilen 
+          aller Kartenebenen (zentrale Steuerung für kleine Bildschirme)
+        </li>
+        <li>
+          <b>Ebenen-Steuerelemente<b>: Kurzbeschreibung (abstract) | Legende | ...
+        </li>
       </ul>
     );
   };
