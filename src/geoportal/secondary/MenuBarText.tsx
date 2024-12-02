a768aa6992ddb6ae19a5a0d9953b5fc460be0696
default <stefan.sander@t-online.de>
2024-12-02 08:55:29 +0000

    Erweiterung Secondary Menüzeile

--- src/geoportal/secondary/MenuBarText.tsx
+++ src/geoportal/secondary/MenuBarText.tsx
@@ -10,20 +10,28 @@ export const MenuBarText = () => {
           Die Menüleiste umfasst mehrere Aktionen und Dialoge zur Steuerung und Weiterverarbeitung der Karteninhalte:
         </div>
         <table>
-            <tr>
-              <td>Icons</td>
-              <td>
-                <b>Kartensteuerelemente aus-/einblenden</b> - Wechselschalter zum Aus- und 
-                Einblenden der <Link target="LAYERBUTTONS">Kartensteuerelemente</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Icon</td>
-              <td>
-                <b>Drucken</b> - Einfacher Ausdruck in PDF-Datei mit Auswahl von Format, Maßstab und Auflösung | 
-                verschiebbarer Druckvorschaurahmen | kein Ausdruck von Legenden, Metadaten etc.
-              </td>
-            </tr>
+          <tr>
+            <td>Icons</td>
+            <td>
+              <b>Hintergrundkarte abschwächen / zurücksetzen</b> - Wechselschalter zum Abschwächen (starke Transparenz) 
+              und Zurücksetzen (keine Transparenz) der <Link target="HINTERGRUND">Hintergrundkarte</Link> | zum Auffinden 
+              von Fachobjekten bei dünn besetzten Kartenebenen
+            </td>
+          </tr>
+          <tr>
+            <td>Icons</td>
+            <td>
+              <b>Kartensteuerelemente aus-/einblenden</b> - Wechselschalter zum Aus- und 
+              Einblenden der <Link target="LAYERBUTTONS">Kartensteuerelemente</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>Icon</td>
+            <td>
+              <b>Drucken</b> - Einfacher Ausdruck in PDF-Datei mit Auswahl von Format, Maßstab und Auflösung | 
+              verschiebbarer Druckvorschaurahmen | kein Ausdruck von Legenden, Metadaten etc.
+            </td>
+          </tr>
         </table>
       </ul>
     );
