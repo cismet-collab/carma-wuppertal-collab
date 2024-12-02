2bd2a264d5d0d92dc8e0d62b071c16dab0d86fc7
default <stefan.sander@t-online.de>
2024-12-02 15:20:06 +0000

    Anpassung Sachdatenabfrage Info-Box korr

--- src/geoportal/secondary/InfoboxText.tsx
+++ src/geoportal/secondary/InfoboxText.tsx
@@ -10,10 +10,10 @@ export const InfoboxText = () => {
         Nur sichtbar in Modi mit Anzeige von Objektinformationen (<b>2D</b>) |{" "} 
         <Link target="MESSUNGEN">Modus Strecke/Fläche messen</Link>: 
         Navigation in zuvor angelegten Messgeometrien, Anzeige der Messergebnisse, Löschen von Messgeometrien 
-        | <b>Modus Multi-Sachdatenabfrage</b>: Anzeige der wichtigsten Informationen/Links zum selektierten 
+        | <b>Modus Sachdatenabfrage</b>: Anzeige der wichtigsten Informationen/Links zum selektierten 
         Objekt oder bei{" "}<Link target="SACHDATENABFRAGE">Multi-Sachdatenabfrage</Link>{" "}zu Objekten an ausgewählter Position, 
         jeweils mit Link zu vollständigen Sachinformationen; Informationen zu mehreren Objekten in überlappenden Info-Boxen, 
-        Info-Box in den Vordergrund holen durch Klicken/Tippen auf den Titelbalken;
+        Info-Box in den Vordergrund holen durch Klicken/Tippen auf den Titelbalken
       </div>
     </ul>
   );
