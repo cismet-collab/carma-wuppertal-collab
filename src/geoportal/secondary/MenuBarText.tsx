fb21d70a5b0abc62c9e3ae5b64670c2249f14047
default <stefan.sander@t-online.de>
2024-12-02 10:10:14 +0000

    Icons Secondary Menüzeile

--- src/geoportal/secondary/MenuBarText.tsx
+++ src/geoportal/secondary/MenuBarText.tsx
@@ -1,3 +1,5 @@
+import focus-on from "./assets/focus-on.png";
+import focus-on from "./assets/focus-on.png";
 import { Link } from "../../helper-overlay/components/Link";
 
 export const MenuBarText = () => {
@@ -11,7 +13,7 @@ export const MenuBarText = () => {
         </div>
         <table>
           <tr>
-            <td>Icons</td>
+            <td><img src={stepOne} style={{ height: "18px" }} /></td>
             <td>
               <b>Hintergrundkarte abschwächen / zurücksetzen</b> - Wechselschalter zum Abschwächen (starke Transparenz) 
               und Zurücksetzen (keine Transparenz) der <Link target="HINTERGRUND">Hintergrundkarte</Link> | zum Auffinden 
