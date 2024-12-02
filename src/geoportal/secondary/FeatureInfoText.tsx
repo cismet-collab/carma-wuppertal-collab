35e883c300d1d5316702ddbe15c90876e09f31f4
default <stefan.sander@t-online.de>
2024-12-02 21:15:15 +0000

    Textangleichung mit Tooltipps

--- src/geoportal/secondary/FeatureInfoText.tsx
+++ src/geoportal/secondary/FeatureInfoText.tsx
@@ -7,7 +7,7 @@ export const FeatureInfoText = () => {
         style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
       >
         <div>
-          Wechselschalter zum An-/Abschalten des Multi-Sachdatenabfragemodus (<b>2D</b>) | 
+          Wechselschalter zum Ein-/Ausschalten des Multi-Sachdatenabfragemodus (<b>2D</b>) | 
           blau unterstrichene <Link target="LAYERBUTTONS">Kartensteuerelemente</Link> kennzeichnen abfragbare Kartenebenen | 
           in Karte klicken/tippen für Abfrage aller sichtbaren "blauen" Kartenebenen an dieser Stelle ("thematischer Durchstich") | 
           Ergebnis- und zusätzliche Koordinatenanzeige in der <Link target="INFOBOX">Info-Box</Link>
