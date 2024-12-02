3f9a3e06df489262cc5deef8d88f8eb56efda746
default <stefan.sander@t-online.de>
2024-12-02 21:47:37 +0000

    Textvereinfachung

--- src/geoportal/secondary/LayerButtonsText.tsx
+++ src/geoportal/secondary/LayerButtonsText.tsx
@@ -7,7 +7,7 @@ export const LayerButtonsText = () => {
         style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
       >
         <div>
-          Festes Hintergrund-Steuerelement wird immer angezeigt, dazu variabel 
+          Festes Hintergrund-Steuerelement, dazu variabel 
           für jede weitere geladene Kartenebene ein Ebenen-Steuerelement (<b>2D</b>) | 
           horizontales Verschieben der Ebenen-Steuerelemente zur Änderung der Darstellungsreihenfolge | 
           Klicken/Tippen auf das Steuerelement zum Öffnen einer Zeile für die Steuerung von 
