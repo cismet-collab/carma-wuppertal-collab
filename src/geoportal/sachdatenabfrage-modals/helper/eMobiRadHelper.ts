68e0d448e6a7bc384ff43b7121bf93fa4469cf55
David Glogaza <david.glogaza40@googlemail.com>
2024-12-06 14:28:48 +0100

    fix modal not working

--- src/geoportal/sachdatenabfrage-modals/helper/eMobiRadHelper.ts
+++ src/geoportal/sachdatenabfrage-modals/helper/eMobiRadHelper.ts
@@ -1,59 +1,58 @@
 export const normalizeInput = (properties: {
-    standort: string;
-    versatz: string;
-    adresse: string;
-    foto: string;
-    bezirk: string;
-    zusatzinfo: string;
-    detailbeschreibung: string;
-    bemerkung: string;
-    anzahl_plaetze: string;
-    anzahl_ladepunkte: string;
-    ladekosten: string;
-    ladebox_zu: string;
-    ladebox_anz: string;
-    ladebox_pfand: string;
-    gruen: string;
-    status: string;
-    verfuegbar:string;
-    betreiber: string;
-    betreiberadresse: string;
-    betreiberort: string;
-    betreiberhomepage: string;
-    betreiberemail: string;
-    betreibertelefon: string;
-    betreiberbemerkung: string;
-    [key: string]: string;
-  }) => {
-    let stecker: string [] = [];
-    let zugangsarten: string[] = [];
-    let oeffnungszeiten = "";
-    for (let key in properties) {
-      if (key.startsWith("typ")) {
-        const value = properties[key];
-        stecker.push(value);
-      }
-  
-      if (key.startsWith("zugang")) {
-        const value = properties[key];
-  
-        zugangsarten.push(value);
-      }
+  standort: string;
+  versatz: string;
+  adresse: string;
+  foto: string;
+  bezirk: string;
+  zusatzinfo: string;
+  detailbeschreibung: string;
+  bemerkung: string;
+  anzahl_plaetze: string;
+  anzahl_ladepunkte: string;
+  ladekosten: string;
+  ladebox_zu: string;
+  ladebox_anz: string;
+  ladebox_pfand: string;
+  gruen: string;
+  status: string;
+  verfuegbar: string;
+  betreiber: string;
+  betreiberadresse: string;
+  betreiberort: string;
+  betreiberhomepage: string;
+  betreiberemail: string;
+  betreibertelefon: string;
+  betreiberbemerkung: string;
+  [key: string]: string;
+}) => {
+  let stecker: string[] = [];
+  let zugangsarten: string[] = [];
+  let oeffnungszeiten = "";
+  for (let key in properties) {
+    if (key.startsWith("typ")) {
+      const value = properties[key];
+      stecker.push(value);
     }
-  
-    if (properties.offen) {
-      if (properties.offen.includes("24-7")) {
-        oeffnungszeiten = "24/7";
-      } else {
-        oeffnungszeiten = properties.offen;
-      }
+
+    if (key.startsWith("zugang")) {
+      const value = properties[key];
+
+      zugangsarten.push(value);
     }
-  
-    return {
-      stecker,
-      zugangsarten,
-      oeffnungszeiten,
-      ...properties,
-    };
+  }
+
+  if (properties.offen) {
+    if (properties.offen.includes("24-7")) {
+      oeffnungszeiten = "24/7";
+    } else {
+      oeffnungszeiten = properties.offen;
+    }
+  }
+
+  return {
+    ...properties,
+    stecker,
+    zugangsarten,
+    oeffnungszeiten,
   };
-  
+};
