c5181f4db9176162eb91db480042a8648ee0647c
default <stefan.sander@t-online.de>
2025-02-02 20:02:07 +0000

    Layout Hintergrundkarte 1

--- src/geoportal/secondary/BackgroundSelectionText.tsx
+++ src/geoportal/secondary/BackgroundSelectionText.tsx
@@ -7,8 +7,9 @@ export const BackgroundSelectionText = () => {
         style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
       >
         <div>
-          Wechselschalter für die Auswahl der Hintergrundkarte (<b>2D</b>) bzw. des 3D-Modells (<b>3D</b>) | 
-          Voreinstellung der 2D-Karte im <Link target="LAYERBUTTONS">Hintergrund-Steuerelement</Link> | 
+          <b></b>Wechselschalter für die Auswahl der Hintergrundkarte (2D) bzw. des 3D-Modells (3D)</b><br/> 
+          Voreinstellung der 2D-Karte im <Link target="LAYERBUTTONS">Hintergrund-Steuerelement</Link>, wird dazu
+          auch direkt rechts neben dem Wechselschalter angeboten | 
           3D: "Karte" basiert auf LoD2-Gebäudemodell der Landesvermessung NRW, "Luftbild" liefert 3D-Mesh
           (berechnet aus Befliegung in 03/24)<br/> 
           <Link section="hintergrundkarte">weitere Informationen</Link>{" "}
