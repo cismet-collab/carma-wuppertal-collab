9ed6bc5b6c0a94937aaa2750c084c33ecc3cb282
default <stefan.sander@t-online.de>
2025-02-08 20:45:20 +0000

    Erweiterung "In Karte positionieren" korr1

--- src/geoportal/help/Help30InKartePositionieren.tsx
+++ src/geoportal/help/Help30InKartePositionieren.tsx
@@ -20,11 +20,11 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                   ein (mindestens 2 Zeichen). In der Auswahlliste werden Ihnen 
                   passende Treffer angeboten. (Wenn Sie weitere Zeichen eingeben, 
                   wird der Inhalt der Auswahlliste angepasst.) Durch das 
-                  vorangestellte Symbol erkennen Sie, ob es sich dabei um einen 
-                  <Icon name="circle" />{" "} Stadtbezirk, ein <Icon name="pie-chart" /> 
-                  Quartier, eine{" "} <Icon name="home" /> Adresse, eine <Icon name="road" /> 
-                  Straße ohne zugeordnete Hausnummern, einen <Icon name="tag" /> 
-                  Interesssanten Ort ("Point of Interest", kurz POI),
+                  vorangestellte Symbol erkennen Sie, ob es sich dabei um einen{" "} 
+                  <Icon name="circle" />{" "} Stadtbezirk, ein <Icon name="pie-chart" />{" "} 
+                  Quartier, eine{" "} <Icon name="home" /> Adresse, eine <Icon name="road" />{" "} 
+                  Straße ohne zugeordnete Hausnummern, einen <Icon name="tag" />{" "} 
+                  Interessanten Ort ("Point of Interest", kurz POI),
                   die <Icon name="tags" /> alternative Bezeichnung eines POI,
                   eine <Icon name="child" /> Kindertageseinrichtung oder eine{" "}
                   <Icon name="graduation-cap" /> Schule handelt.
--- src/geoportal/secondary/GazetteerText.tsx
+++ src/geoportal/secondary/GazetteerText.tsx
@@ -7,8 +7,8 @@ export const GazetteerText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        2-stufiges Eingabeelement für Positionierung über Adresse oder andere Ortsbezeichnung 
-        (<b>2D</b> und <b>3D</b>) | Text eingeben (min. 2 Zeichen) für unscharfe Suche im Verzeichnis der Ortsnamen | 
+        <b>2-stufiges Eingabeelement für Positionierung über Adresse oder andere Ortsbezeichnung 
+        (2D und 3D)</b><br/> Text eingeben für unscharfe Suche im Verzeichnis der Ortsnamen | 
         Treffer werden nach Grad der Übereinstimmung mit Suchtext klassifiziert und gruppiert | 
         Eintrag in Trefferliste anklicken zur Zentrierung der Karte auf diesen Ort | 
         Ortsmarkierung in Karte durch Placemark-Symbol oder (bei Flächen) Spotlight-Effekt<br/>  
