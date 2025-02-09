95078bb8913fbe3553d414fbd8d85dd03573767f
default <stefan.sander@t-online.de>
2025-02-09 16:21:21 +0000

    Überschriften korr. 4

--- src/geoportal/secondary/HomeText.tsx
+++ src/geoportal/secondary/HomeText.tsx
@@ -5,7 +5,7 @@ export const HomeText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        <b></b>Klicken für Rückkehr zum Ausgangspunkt (2D und 3D)</b><br/> 
+        <b>Klicken für Rückkehr zum Ausgangspunkt (2D und 3D)</b><br/> 
         feste  Voreinstellung: Rathaus Wuppertal-Barmen | im 3D-Modus als Schrägansicht  
       </div>
     </ul>
