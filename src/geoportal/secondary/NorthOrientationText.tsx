a99fc069973a03f6c3144d3d084077bedc4711e2
default <stefan.sander@t-online.de>
2025-02-09 15:59:47 +0000

    Überschriften in den Secondary-Elementen 2

--- src/geoportal/secondary/NorthOrientationText.tsx
+++ src/geoportal/secondary/NorthOrientationText.tsx
@@ -7,7 +7,7 @@ export const NorthOrientationText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        <b>3D</b>-Darstellung nach Norden ausrichten und zurück in die Senkrechtperspektive kippen | 
+        <b>3D-Darstellung nach Norden ausrichten und zurück in die Senkrechtperspektive kippen</b><br/>  
         kartenartige Darstellung im 3D-Modus | automatische Ausführung beim{" "}  
         <Link target="2D_3D_TOGGLE">Wechsel von 3D auf 2D</Link>
       </div>
--- src/geoportal/secondary/Toggle2d3dText.tsx
+++ src/geoportal/secondary/Toggle2d3dText.tsx
@@ -5,7 +5,7 @@ export const Toggle2d3dText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        Wechselschalter zum Umschalten zwischen <b>2D</b>- und <b>3D</b>-Ansicht | 
+        <b>Wechselschalter zum Umschalten zwischen 2D- und 3D-Ansicht</b><br/>  
         aktuelle Einstellung Karte/Luftbild wird beim Umschalten beibehalten | 
         bei Wechsel von 3D nach 2D wird Darstellung zuerst nach Norden 
         ausgerichtet und Senkrechtperspektive eingestellt (kartenartige Darstellung)
