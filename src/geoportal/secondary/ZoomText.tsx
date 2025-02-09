11c1883b6a4ca0b41182fee844176fcd9f1a2f58
default <stefan.sander@t-online.de>
2025-02-09 16:37:33 +0000

    Überschriften korr. 5

--- src/geoportal/secondary/ZoomText.tsx
+++ src/geoportal/secondary/ZoomText.tsx
@@ -5,9 +5,9 @@ export const ZoomText = () => {
         style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
       >
         <div>
-        Klicken zum Vergrößern („Zoom in“) bzw. Verkleinern („Zoom out“) des Kartenmaßstabs 
-        in festen Zoomstufen (<b>2D</b> und <b>3D</b>) | "+" für Zoom in, "-" für Zoom out | 
-        je größer die Zoomstufe, desto größer der Maßstab  
+          <b>Klicken zum Vergrößern („Zoom in“) bzw. Verkleinern („Zoom out“)</b><br/>
+          Veränderung des Kartenmaßstabs in festen Zoomstufen (2D und 3D) | 
+          "+" für Zoom in, "-" für Zoom out | je größer die Zoomstufe, desto größer der Maßstab  
         </div>
       </ul>
     );
