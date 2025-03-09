27729b423c34f494f9a6243b3060862f874e4548
default <stefan.sander@t-online.de>
2025-03-09 22:26:38 +0000

    Überarbeitung Abschnitt zum Gazetteer

--- src/geoportal/help/Help30InKartePositionieren.tsx
+++ src/geoportal/help/Help30InKartePositionieren.tsx
@@ -16,11 +16,15 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
             pretext={
               <>
                 <p>
-                  Um eine bestimmte Stelle des Stadtgebietes zu erkunden, geben
-                  Sie den Namen dieses Ortes im Eingabefeld links unten ein
-                  (mindestens 2 Zeichen). In der Auswahlliste werden Ihnen
-                  passende Treffer angeboten. (Wenn Sie weitere Zeichen
-                  eingeben, wird der Inhalt der Auswahlliste angepasst.) Durch
+                  Um eine bestimmte Stelle des Stadtgebietes zu
+                  erkunden, geben Sie den Namen eines Stadtteils
+                  (Stadtbezirk oder Quartier), einer Adresse,
+                  eines Straßennamens oder eines POI im
+                  Eingabefeld links unten ein. In der 
+                  inkrementellen Auswahlliste werden Ihnen schon 
+                  nach der Eingabe des ersten Buchstabens passende 
+                  Treffer angeboten. (Wenn Sie weitere Zeichen eingeben, 
+                  wird der Inhalt der Auswahlliste angepasst.) Durch
                   das vorangestellte Symbol erkennen Sie, ob es sich dabei um
                   einen <Icon name="circle" /> Stadtbezirk, ein{" "}
                   <Icon name="pie-chart" /> Quartier, eine <Icon name="home" />{" "}
