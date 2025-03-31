9c1d8252593b42d15bfc539bdd932d2d08e2ed23
Thorsten Hell <thorsten.hell@cismet.de>
2025-03-31 11:02:41 +0200

    add custom hintergrundkarten prop

--- src/fnp-inspektor/Footer.tsx
+++ src/fnp-inspektor/Footer.tsx
@@ -14,6 +14,19 @@ export const Footer = ({
       title="FNP-Inspektor Wuppertal"
       version={version}
       setAppMenuActiveMenuSection={setAppMenuActiveMenuSection}
+      hintergrundkarten={
+        <span>
+          <a
+            target="_rvrReference"
+            rel="noopener noreferrer"
+            href="https://www.rvr.ruhr/?id=1002#c179626"
+          >
+            Stadtkarte 2.0 © RVR
+          </a>{" "}
+          | Deutsche Grundkarte (DGK 5) Stand 1996 © Geobasis NRW |
+          Flächennutzungsplan vom 17.01.2005 © Stadt Wuppertal
+        </span>
+      }
     />
   );
 };
