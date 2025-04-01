012c00d55e572715f6e10aacaa0b4e872878236c
default <stefan.sander@t-online.de>
2025-04-01 16:17:56 +0000

    Neues Luftbild Hitzebelastungskarte

--- src/hitzeinderstadt/hitze/Help10Datengrundlage.tsx
+++ src/hitzeinderstadt/hitze/Help10Datengrundlage.tsx
@@ -76,6 +76,50 @@ export const Help10Datengrundlage = () => {
 
           <ul>
             <li>
+              <strong>Stadtplan</strong>: Kartendienst (WMS) des
+              Regionalverbandes Ruhr (RVR). Datengrundlage:{" "}
+              <strong>Stadtkarte 2.0</strong>. Wöchentlich in einem
+              automatischen Prozess aktualisierte Zusammenführung des
+              Straßennetzes der OpenStreetMap mit Amtlichen Geobasisdaten des
+              Landes NRW aus den Fachverfahren ALKIS (Gebäude, Flächennutzungen)
+              und ATKIS (Gewässer). © RVR und Kooperationspartner (
+              <a target="_legal" href="https://www.govdata.de/dl-de/by-2-0">
+                Datenlizenz Deutschland - Namensnennung - Version 2.0
+              </a>
+              ). Lizenzen der Ausgangsprodukte:{" "}
+              <a target="_legal" href="https://www.govdata.de/dl-de/zero-2-0">
+                Datenlizenz Deutschland - Zero - Version 2.0
+              </a>{" "}
+              (Amtliche Geobasisdaten) und{" "}
+              <a
+                target="_legal"
+                href="https://www.opendatacommons.org/licenses/odbl/1.0/"
+              >
+                ODbL
+              </a>{" "}
+              (OpenStreetMap contributors).
+            </li>
+            <li>
+              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) der Stadt
+              Wuppertal. Datengrundlage:{" "}
+              <strong>True Orthophoto aus Bildflügen vom 14. und 17.03.2024</strong>, 
+              hergestellt durch Aerowest GmbH/Dortmund, Bodenauflösung 5 cm. 
+              (True Orthophoto: Aus Luftbildern mit hoher Längs- und Querüberdeckung 
+              in einem automatisierten Bildverarbeitungsprozess berechnetes Bild in
+              Parallelprojektion, also ohne Gebäudeverkippung und sichttote Bereiche.)
+              © Stadt Wuppertal (
+              <a
+                target="_legal"
+                href="https://www.wuppertal.de/geoportal/Nutzungsbedingungen/NB-GDIKOM-C_Geodaten.pdf"
+              >
+                NB-GDIKOM C
+              </a>
+              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR).
+              Datengrundlagen: <strong>Stadtkarte 2.0</strong> und{" "}
+              <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. (Details s.
+              Hintergrundkarte Stadtplan).
+            </li>
+            <li>
               <strong>Topographische Karte</strong>: (1) Kartendienste (WMS) der
               Stadt Wuppertal. Datengrundlagen: (a){" "}
               <b>Amtliche Basiskarte ABK Graustufen</b>. (Wöchentlich in einem
@@ -110,52 +154,6 @@ export const Help10Datengrundlage = () => {
                 CC BY 4.0
               </a>
               .
-            </li>
-
-            <li>
-              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) der Stadt
-              Wuppertal. Datengrundlage:{" "}
-              <strong>True Orthophoto aus Bildflug vom 16.03.2022</strong>,
-              hergestellt durch Aerowest GmbH/Dortmund, Bodenauflösung 5 cm.
-              (True Orthophoto: Aus Luftbildern mit hoher Längs- und
-              Querüberdeckung in einem automatisierten Bildverarbeitungsprozess
-              berechnetes Bild in Parallelprojektion, also ohne
-              Gebäudeverkippung und sichttote Bereiche.) © Stadt Wuppertal (
-              <a
-                target="_legal"
-                href="https://www.wuppertal.de/geoportal/Nutzungsbedingungen/NB-GDIKOM-C_Geodaten.pdf"
-              >
-                NB-GDIKOM C
-              </a>
-              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR).
-              Datengrundlagen: <strong>Stadtkarte 2.0</strong> und{" "}
-              <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. (Details s.
-              Hintergrundkarte Stadtplan).
-            </li>
-
-            <li>
-              <strong>Stadtplan</strong>: Kartendienst (WMS) des
-              Regionalverbandes Ruhr (RVR). Datengrundlage:{" "}
-              <strong>Stadtkarte 2.0</strong>. Wöchentlich in einem
-              automatischen Prozess aktualisierte Zusammenführung des
-              Straßennetzes der OpenStreetMap mit Amtlichen Geobasisdaten des
-              Landes NRW aus den Fachverfahren ALKIS (Gebäude, Flächennutzungen)
-              und ATKIS (Gewässer). © RVR und Kooperationspartner (
-              <a target="_legal" href="https://www.govdata.de/dl-de/by-2-0">
-                Datenlizenz Deutschland - Namensnennung - Version 2.0
-              </a>
-              ). Lizenzen der Ausgangsprodukte:{" "}
-              <a target="_legal" href="https://www.govdata.de/dl-de/zero-2-0">
-                Datenlizenz Deutschland - Zero - Version 2.0
-              </a>{" "}
-              (Amtliche Geobasisdaten) und{" "}
-              <a
-                target="_legal"
-                href="https://www.opendatacommons.org/licenses/odbl/1.0/"
-              >
-                ODbL
-              </a>{" "}
-              (OpenStreetMap contributors).
             </li>
           </ul>
         </div>
