3c9d84273011518e8a63538dad475c75a372cc55
default <stefan.sander@t-online.de>
2025-04-05 08:06:11 +0000

    Bäderkarte Gazetteer /1

--- src/baederkarte/KompaktanleitungSection.jsx
+++ src/baederkarte/KompaktanleitungSection.jsx
@@ -226,7 +226,7 @@ export const KompaktanleitungSection = ({
                               ein. In der inkrementellen Auswahlliste werden 
                               Ihnen schon nach der Eingabe des ersten Buchstabens 
                               passende Treffer angeboten. (Wenn Sie weitere Zeichen 
-                              eingeben, wird der Inhalt der Auswahlliste angepasst.
+                              eingeben, wird der Inhalt der Auswahlliste angepasst.)
                               Durch das vorangestellte Symbol erkennen
                               Sie, ob es sich dabei um einen{" "}
                               <Icon name="circle" /> Stadtbezirk, ein{" "}
