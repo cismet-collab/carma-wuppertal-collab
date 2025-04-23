228777edc1565855e4a7a6761fdb4309a196d8a9
default <stefan.sander@t-online.de>
2025-04-23 08:51:35 +0000

    Fuzzy Search Gazetteer GenericTopicMap

--- src/generic-topicmap/docBlocks/InKartePositionieren.tsx
+++ src/generic-topicmap/docBlocks/InKartePositionieren.tsx
@@ -1,4 +1,5 @@
 import Icon from "react-cismap/commons/Icon";
+import FuzzySearchParagraph from "../../commons/FuzzySearchParagraph";
 
 const NW = (props) => {
   return <span style={{ whiteSpace: "nowrap" }}>{props.children}</span>;
@@ -44,25 +45,25 @@ export const InKartePositionieren = ({
     </p>
   ),
   listAddendum = null,
-  grosseZoomstufe = 14,
+  grosseZoomstufe = 18,
   addendum = null,
 }) => {
   return (
     <div>
       <p>
         Um in der Karte direkt zu einer bestimmten Adresse zu gelangen, geben
-        Sie den Anfang des betreffenden Straßennamens im Eingabefeld links unten
-        ein (mindestens 2 Zeichen). In der inkrementellen Auswahlliste werden
-        Ihnen passende Treffer angeboten. (Wenn Sie weitere Zeichen eingeben,
-        wird der Inhalt der Auswahlliste angepasst.) Sie können auch andere
-        Suchbegriffe eingeben, nämlich Stadtteil (Stadtbezirk oder Quartier),
-        Straßenname (bei Straßen ohne zugeordnete Hausnummern) oder &quot;Point
-        of Interest&quot; (interessanter Ort, kurz als POI bezeichnet).
+        Sie diese im Eingabefeld links unten ein. In der inkrementellen Auswahlliste
+        werden Ihnen schon nach der Eingabe des ersten Buchstabens passende Treffer 
+        angeboten. (Wenn Sie weitere Zeichen eingeben, wird der Inhalt der Auswahlliste 
+        angepasst.) Sie können auch andere Suchbegriffe eingeben, nämlich Stadtteil 
+        (Stadtbezirk oder Quartier), Straßenname (bei Straßen ohne zugeordnete Hausnummern) 
+        oder &quot;Point of Interest&quot; (interessanter Ort, kurz als POI bezeichnet).
       </p>
       {listWithSymbols}
 
       {listAddendum && <p>{listAddendum}</p>}
-
+      
+      <FuzzySearchParagraph />
       <p>
         Nach der Auswahl eines Treffers aus der Liste wird die Karte auf die
         zugehörige Position zentriert. Bei Suchbegriffen mit Punktgeometrie
