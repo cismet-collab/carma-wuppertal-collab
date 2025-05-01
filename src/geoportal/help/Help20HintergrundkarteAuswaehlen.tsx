b61c51e2e6f3fb14225382c44f3c5c2643d612bc
default <stefan.sander@t-online.de>
2025-05-01 12:37:35 +0000

    ChatGPT-Text für Hintergrundkarte

--- src/geoportal/help/Help20HintergrundkarteAuswaehlen.tsx
+++ src/geoportal/help/Help20HintergrundkarteAuswaehlen.tsx
@@ -12,42 +12,39 @@ const Component = ({ showOverlayFromOutside }) => {
       sectionContent={
         <div>
           <p>
-            Mit dem Wechselschalter Karte / Luftbild rechts oben können Sie
-            jederzeit zwischen einer klassischen Karte und einer Luftbildkarte
-            als Hintergrundkarte wechseln. Durch Anklicken des
-            Hintergrund-Steuerelements <FontAwesomeIcon icon={faLayerGroup} />{" "}
-            rechts neben dem Wechselschalter öffnen Sie einen Dialog mit einer
-            größeren Auswahl an Karten bzw. Luftbildkarten. Im aufgeklappten
-            Zustand dieses Dialogs werden zusätzlich Informationen zur Eignung
-            und zum Inhalt der jeweils selektierten Karte angezeigt. Die von
-            Ihnen hier ausgewählte Hintergrundkarte wird sofort geladen und
-            zusätzlich als Belegung für den Wechselschalter gespeichert. Wenn
-            Sie den Mauszeiger über die beiden Schaltflächen des
-            Wechselschalters bewegen, werden die aktuell eingestellten
-            Hintergrundkarten als Tooltip angezeigt.
+            Mit dem Wechselschalter "Karte / Luftbild" oben rechts können Sie 
+            jederzeit zwischen einer klassischen Karte und einer 
+            Luftbildkarte wechseln. Wenn Sie auf das 
+            Hintergrund-Steuerelement <FontAwesomeIcon icon={faLayerGroup} />{" "} 
+            neben dem Wechselschalter klicken, öffnet sich ein Dialog, in dem Sie aus 
+            weiteren Karten und Luftbildkarten auswählen können. Wenn dieser 
+            Dialog aufgeklappt ist, sehen Sie auch Informationen darüber, wofür 
+            diese Karten geeignet sind und was sie enthalten. Die Hintergrundkarte, 
+            die Sie auswählen, wird sofort geladen und als Einstellung für den 
+            Wechselschalter gespeichert. Wenn Sie mit dem Mauszeiger über die 
+            Schaltflächen des Wechselschalters fahren, wird Ihnen die aktuell 
+            eingestellte Karte als Hinweis angezeigt. 
           </p>
           <p>
-            Das identische Hintergrund-Steuerelement finden Sie auch als erstes
-            (linkes) Kartensteuerelement oben im Kartenfenster. Es dient nämlich
-            auch als zentrales Steuerelement für alle geladenen Kartenebenen,
-            was vor allem für kleine Bildschirme vorteilhaft ist.
+            Das gleiche Hintergrund-Steuerelement finden Sie auch oben im 
+            Kartenfenster als erstes (linkes) Kartensteuerelement. Es dient 
+            nämlich auch als zentrales Steuerelement für alle geladenen Kartenebenen, 
+              was vor allem auf kleinen Bildschirmen nützlich ist. 
           </p>
           <p>
-            Im 3D-Modus bieten wir Ihnen über den Wechselschalter Karte /
-            Luftbild zwei fest eingestellte 3D-Modelle an. Mit der Schaltfläche
-            "Karte" laden Sie den Wuppertaler Ausschnitt des für ganz NRW
-            vorliegenden 3D-Gebäudemodells der Landesvermessung NRW (Geobasis
-            NRW) in der inhaltlichen Ausbaustufe "Level of Detail 2 (LoD2)". Das
-            sind einfache Klötzchen mit standardisierten Dachformen. Mit
-            "Luftbild" laden Sie ein 3D-Mesh, das weitestgehend automatisiert
-            aus den Bilddaten der im Auftrag der Stadt Wuppertal im März 2024
-            durchgeführten Befliegung abgeleitet wurde. Für ein 3D-Mesh wird aus
-            den Bilddaten ein Digitales Oberflächenmodell in Form eines
-            Dreiecksnetzes berechnet, auf das dann die Bildinhalte projiziert
-            werden. Solche Modelle enthalten beim gegenwärtigen Stadt der
-            Technik regelmäßig Lücken und falsche Darstellungen, vor allem dort,
-            wo mehrere Ebenen vertikal übereinander liegen (z. B. bei den
-            Schwebebahnhöfen).
+            Im 3D-Modus können Sie mit dem Schalter "Karte / Luftbild" zwei verschiedene 
+            3D-Modelle auswählen. Mit der Schaltfläche "Karte" laden Sie ein einfaches 
+            3D-Modell von Wuppertal, das aus einem größeren Modell für ganz NRW erstellt 
+            wurde. Es stellt einen Ausschnitt des für ganz Nordrhein-Westfalen vorliegenden 
+            3D-Gebäudemodells der Landesvermessung NRW (Geobasis NRW) in der inhaltlichen 
+            Ausbaustufe "Level of Detail 2 (LoD2)" dar. Dieses Modell umfasst einfache 
+            Gebäudeformen mit standardisierten Dachformen. Mit der Schaltfläche "Luftbild" l
+            aden Sie ein 3D-Mesh, das aus Bilddaten erstellt wurde, die durch eine Befliegung 
+            im Auftrag der Stadt Wuppertal im März 2024 gewonnen wurden. Dabei wird aus den 
+            Bilddaten ein digitales Oberflächenmodell in Form eines Dreiecksnetzes berechnet, 
+            auf das die Bilder projiziert werden. Solche Modelle können Fehler und Lücken 
+            enthalten, vor allem dort, wo verschiedene Ebenen übereinander liegen, wie zum 
+            Beispiel bei den Schwebebahnhöfen.
           </p>
           <p className="hide-on-small-screens">
             <Link
