e9d9e42f5515278042ba9bff6c10d464f32babbb
default <stefan.sander@t-online.de>
2025-05-05 18:42:29 +0000

    Hilfeabschnitt Karteninhalte hinzufügen /4

--- src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
+++ src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
@@ -1,4 +1,5 @@
 import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
+import { Link } from "../../helper-overlay/components/Link";
 import addLayers from "../secondary/assets/add-layers.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +8,7 @@ import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-const Component = () => {
+const Component = ({ showOverlayFromOutside }) => {
   return (
     <GenericModalMenuSection
       sectionKey="karteninhalte"
@@ -15,34 +16,45 @@ const Component = () => {
       sectionBsStyle="info"
       sectionContent={
         <div>
-          Das Herzstück des Geoportals ist der Dialog "Karteninhalte hinzufügen" 
-          (Symbol {" "}<img src={addLayers} style={{ height: "18px" }} />{" "} 
-          in der Menüleiste), mit dem Sie das gesamte Datenangebot des DigiTal 
-          Zwillings durchsuchen und für Sie interessante Inhalte als Kartenebene 
-          hinzufügen können. Um Ihnen die Orientierung in dem umfangreichen Angebot 
-          zu erleichtern, sind die Karteninhalte in vertikaler Richtung nach Art 
-          und in horizontaler Richtung nach Thema geordnet. Wenn Sie also im 
-          Navigationsbereich auf der linken Seite z. B. auf 
-          {" "}<FontAwesomeIcon icon={faMap} />{" "} <strong>Kartenebenen</strong>{" "} 
-          klicken, werden Vorschaubilder zu allen Kartenebenen in den Hauptbereich 
-          des Dialogs geladen. Wenn Sie dann in der horizontalen Navigationsleiste 
-          im Kopfbereich z. B. auf "Planung" klicken, wird im Hauptbereich auf den 
-          Beginn des Blocks der Kartenebenen zum Thema "Planung" positioniert.
-          Ausgehend von einem Vorschaubild können Sie entweder durch Anklicken des 
-          Wechselschalters {" "}<FontAwesomeIcon icon={faChevronDown} />{" "} bzw. 
-          {" "}<FontAwesomeIcon icon={faChevronUp} />{" "} einen Bereich mit 
-          Informationen zu der Kartenebene auf- bzw. zuklappenklappen, der auch Schaltflächen 
-          zum Hinzufügen und Favorisieren dieser Kartenebene enthält. Oder Sie benutzen 
-          hierfür direkt die Symbole {" "}<FontAwesomeIcon icon={faPlus} />{" "} 
-          und {" "}<FontAwesomeIcon icon={faStar} />{" "} im Vorschaubild. 
-          "Hinzufügen" bewirkt, dass diese Kartenebene im Geoportal direkt geladen 
-          und angezeigt wird. Außerdem wird für sie ein weiteres Ebenen-Steuerelement 
-          rechts neben dem Hintergrund-Steuerelement ergänzt. Durch "Favorisieren" wird 
-          die Kartenebene in Ihrem Favoriten-Bereich unter "Meine Kartenebenen" 
-          gespeichert. Zu Ihren Favoriten gelangen Sie, indem Sie oben im Navigationsbereich 
-          auf {" "}<FontAwesomeIcon icon={faStar} />{" "} <strong>Favoriten</strong>{" "} klicken. 
-          Das Speichen der Favoriten benutzt den Cache-Speicher Ihres Browsers. Wenn Sie diesen 
-          Speicher löschen, verschwinden auch die dort gespeicherten Karteninhalte.
+          <p>
+            Das Herzstück des Geoportals ist der Dialog "Karteninhalte hinzufügen" 
+            (Symbol {" "}<img src={addLayers} style={{ height: "18px" }} />{" "} 
+            in der Menüleiste), mit dem Sie das gesamte Datenangebot des DigiTal 
+            Zwillings durchsuchen und für Sie interessante Inhalte als Kartenebene 
+            hinzufügen können. Um Ihnen die Orientierung in dem umfangreichen Angebot 
+            zu erleichtern, sind die Karteninhalte in vertikaler Richtung nach Art 
+            und in horizontaler Richtung nach Thema geordnet. Wenn Sie also im 
+            Navigationsbereich auf der linken Seite z. B. auf 
+            {" "}<FontAwesomeIcon icon={faMap} />{" "} <strong>Kartenebenen</strong>{" "} 
+            klicken, werden Vorschaubilder zu allen Kartenebenen in den Hauptbereich 
+            des Dialogs geladen. Wenn Sie dann in der horizontalen Navigationsleiste 
+            im Kopfbereich z. B. auf "Planung" klicken, wird im Hauptbereich auf den 
+            Beginn des Blocks der Kartenebenen zum Thema "Planung" positioniert.
+            Ausgehend von einem Vorschaubild können Sie entweder durch Anklicken des 
+            Wechselschalters {" "}<FontAwesomeIcon icon={faChevronDown} />{" "} bzw. 
+            {" "}<FontAwesomeIcon icon={faChevronUp} />{" "} einen Bereich mit 
+            Informationen zu der Kartenebene auf- bzw. zuklappen, der auch Schaltflächen 
+            zum Hinzufügen und Favorisieren dieser Kartenebene enthält. Oder Sie benutzen 
+            hierfür direkt die Symbole {" "}<FontAwesomeIcon icon={faPlus} />{" "} 
+            und {" "}<FontAwesomeIcon icon={faStar} />{" "} im Vorschaubild. 
+            "Hinzufügen" bewirkt, dass diese Kartenebene im Geoportal direkt geladen 
+            und angezeigt wird. Außerdem wird für sie ein weiteres Ebenen-Steuerelement 
+            rechts neben dem Hintergrund-Steuerelement ergänzt. Durch "Favorisieren" wird 
+            die Kartenebene in Ihrem Favoriten-Bereich unter "Meine Kartenebenen" 
+            gespeichert. Zu Ihren Favoriten gelangen Sie, indem Sie oben im Navigationsbereich 
+            auf {" "}<FontAwesomeIcon icon={faStar} />{" "} <strong>Favoriten</strong>{" "} klicken. 
+            Das Speichen der Favoriten benutzt den Cache-Speicher Ihres Browsers. Wenn Sie diesen 
+            Speicher löschen, verschwinden auch die dort gespeicherten Karteninhalte.
+          </p>
+          <p className="hide-on-small-screens">
+            <Link
+              onClick={() => {
+                showOverlayFromOutside("MENU");
+              }}
+            >
+              zur Hilfefolie
+            </Link>
+          </p>
         </div>
       }
     />
