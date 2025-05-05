ba062481633808ceb7ffbc16cc1a5b0b18b3116f
default <stefan.sander@t-online.de>
2025-05-05 15:55:01 +0000

    Hilfeabschnitt Karteninhalte hinzufügen /2

--- src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
+++ src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
@@ -2,6 +2,10 @@ import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
 import addLayers from "../secondary/assets/add-layers.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Component = () => {
   return (
@@ -19,11 +23,21 @@ const Component = () => {
           zu erleichtern, sind die Karteninhalte in vertikaler Richtung nach Art 
           und in horizontaler Richtung nach Thema geordnet. Wenn Sie also im 
           Navigationsbereich auf der linken Seite z. B. auf 
-          {" "}<FontAwesomeIcon icon={faMap} />{" "} <strong>Kartenebenen</strong> 
+          {" "}<FontAwesomeIcon icon={faMap} />{" "} <strong>Kartenebenen</strong>{" "} 
           klicken, werden Vorschaubilder zu allen Kartenebenen in den Hauptbereich 
           des Dialogs geladen. Wenn Sie dann in der horizontalen Navigationsleiste 
           im Kopfbereich z. B. auf "Planung" klicken, wird im Hauptbereich auf den 
           Beginn des Blocks der Kartenebenen zum Thema "Planung" positioniert.
+          Ausgehend von einem Vorschaubild können Sie entweder durch Anklicken des 
+          Wechselschalters {" "}<FontAwesomeIcon icon={faChevronDown} />{" "} bzw. 
+          {" "}<FontAwesomeIcon icon={faChevronUp} />{" "} einen Bereich mit 
+          Informationen zu der Kartenebene aufklappen, der auch Schaltflächen zum 
+          Hinzufügen und Favorisieren dieser Kartenebene enthält. Oder Sie benutzen 
+          hierfür direkt die Symbole {" "}<FontAwesomeIcon icon={faPlus} />{" "} 
+          und {" "}<FontAwesomeIcon icon={faStar} />{" "} im Vorschaubild. 
+          "Hinzufügen" bewirkt, dass diese Kartenebene im Geoportal direkt geladen 
+          und angezeigt wird. Außerdem wird für diese Kartenebene ein weiteres 
+          Ebenen-Steuerelement rechts neben dem Hintergrund-Steuerelement ergänzt. 
         </div>
       }
     />
