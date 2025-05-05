82ee9a9494256444bcda3f381d761e8e047d8247
default <stefan.sander@t-online.de>
2025-05-05 14:04:51 +0000

    Hilfeabschnitt Karteninhalte hinzufügen /1

--- src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
+++ src/geoportal/help/Help35KarteninhalteHinzufuegen.tsx
@@ -1,5 +1,6 @@
 import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
 import addLayers from "../secondary/assets/add-layers.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Component = () => {
   return (
@@ -10,15 +11,16 @@ const Component = () => {
       sectionContent={
         <div>
           Das Herzstück des Geoportals ist der Dialog "Karteninhalte hinzufügen" 
-          (Symbol {" "}<img src={addLayers} style={{ height: "18px" }} />{" "} in der Menüleiste), 
-          mit dem Sie das gesamte Datenangebot des DigiTal Zwillings durchsuchen 
-          und für Sie interessante Inhalte als Kartenebene hinzufügen können. Um 
-          Ihnen die Orientierung in dem umfangreichen Angebot zu erleichtern, sind 
-          die Karteninhalte in vertikaler Richtung nach ihrer Art und in horizontaler 
-          Richtung nach ihrem Thema geordnet. Wenn Sie also im Navigationsbereich 
-          auf der linken Seite z. B. auf Icon "Kartenebenen" klicken, werden 
-          Vorschaubilder zu allen Kartenebenen in den Hauptbereich des Dialogs 
-          geladen. Wenn Sie dann in der horizontalen Navigationsleiste 
+          (Symbol {" "}<img src={addLayers} style={{ height: "18px" }} />{" "} 
+          in der Menüleiste), mit dem Sie das gesamte Datenangebot des DigiTal 
+          Zwillings durchsuchen und für Sie interessante Inhalte als Kartenebene 
+          hinzufügen können. Um Ihnen die Orientierung in dem umfangreichen Angebot 
+          zu erleichtern, sind die Karteninhalte in vertikaler Richtung nach Art 
+          und in horizontaler Richtung nach Thema geordnet. Wenn Sie also im 
+          Navigationsbereich auf der linken Seite z. B. auf 
+          {" "}<FontAwesomeIcon icon="faMap" />{" "} <strong>Kartenebenen</strong> 
+          klicken, werden Vorschaubilder zu allen Kartenebenen in den Hauptbereich 
+          des Dialogs geladen. Wenn Sie dann in der horizontalen Navigationsleiste 
           im Kopfbereich z. B. auf "Planung" klicken, wird im Hauptbereich auf den 
           Beginn des Blocks der Kartenebenen zum Thema "Planung" positioniert.
         </div>
