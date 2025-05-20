649f20b8decbbff6f2b0b25eab1f98b29b3d0c81
default <stefan.sander@t-online.de>
2025-05-20 15:11:26 +0000

    Hintergrund UDZ /6

--- src/geoportal/help/Help10DigitalerZwilling.jsx
+++ src/geoportal/help/Help10DigitalerZwilling.jsx
@@ -11,18 +11,23 @@ const Component = () => {
       sectionContent=<GenericDigitalTwinReferenceTextComponent pretext={
         <>
           Willkommen in der Portalkomponente zum <strong>DigiTal Zwilling</strong>, 
-          dem{" "} <a href="https://smart.wuppertal.de/projekte/digital-zwilling.php">
-          Urbanen Digitalen Zwilling der Stadt Wuppertal!</a>{" "} Hier haben Sie Zugang 
+          dem{" "} <a target="_legal" href="https://smart.wuppertal.de/projekte/digital-zwilling.php">
+          Urbanen Digitalen Zwilling der Stadt Wuppertal</a>! Hier haben Sie Zugang 
           zu allen Datenquellen des DigiTal Zwillings und allen allgemeinen 
           Visualisierungsmethoden und Funktionen, die für eine größere Anzahl von 
           Datenquellen und Anwendungssituationen von Nutzen sind. Zum DigiTal Zwilling 
           gehören auch die sog. Teilzwillinge. Dies sind Anwendungen, die einen 
           themenspezifischen Ausschnitt aus den Daten und Methoden des DigiTal Zwillings 
           für einen spezifischen Anwendungsfall darstellen, z. B. die{" "} 
-          <a href="https://digital-twin-wuppertal-live.github.io/rainhazardmap/">Starkregengefahrenkarte</a>. 
-          In den Teilzwillingen finden Sie weitergehende, spezielle Visualisierungsmethoden 
-          und Funktionen, die wir Geoportal nicht anbieten. Zu den verfügbaren Teilzwillingen 
-          gelangen Sie über den Dialog Karteninhalte hinzufügen. 
+          <a target="_legal" href="https://digital-twin-wuppertal-live.github.io/rainhazardmap/">
+          Starkregengefahrenkarte</a>. In den Teilzwillingen finden Sie weitergehende, spezielle 
+          Visualisierungsmethoden und Funktionen, die wir imGeoportal nicht anbieten. Zu den 
+          verfügbaren Teilzwillingen gelangen Sie über den{" "} 
+          <a 
+              className="renderAsLink" 
+              onClick={() => setAppMenuActiveMenuSection("karteninhalte")}>
+                Dialog Karteninhalte hinzufügen
+          </a>. 
         </>
         } 
       />
