a12748fdc4b74aa24e23d5a1f0d942f4ab08a911
David Glogaza <david.glogaza40@googlemail.com>
2025-05-20 09:32:30 +0200

    remove line break

--- src/geoportal/help/Help05Introduction.tsx
+++ src/geoportal/help/Help05Introduction.tsx
@@ -3,17 +3,16 @@ import { Link } from "../../helper-overlay/components/Link";
 const Help05Introduction = ({ showOverlayFromOutside }) => {
   return (
     <span>
-      An dieser Stelle entsteht bis Ende 2026 in einem mehrstufigen 
-      Entwicklungsprozess die Portalkomponente zum Urbanen Digitalen 
-      Zwilling der Stadt Wuppertal (<strong>DigiTal Zwilling / Geoportal</strong>). 
-      Die vorliegende Ausbaustufe 1 hat ihren Schwerpunkt noch bei der 
-      klassischen Aufgabe eines kommunalen Geoportals, einen digitalen 
-      Kartenatlas der Stadt in Form eines Baukastens kombinierbarer 
-      Kartenebenen bereitzustellen. Wählen Sie eine der folgenden farbigen 
-      Schaltflächen, um sich weitere Bedienungs- und Hintergrundinformationen 
-      zu dem entsprechenden Thema anzeigen zu lassen.
-      <br/>
-      <p className="hide-on-small-screens">
+      An dieser Stelle entsteht bis Ende 2026 in einem mehrstufigen
+      Entwicklungsprozess die Portalkomponente zum Urbanen Digitalen Zwilling
+      der Stadt Wuppertal (<strong>DigiTal Zwilling / Geoportal</strong>). Die
+      vorliegende Ausbaustufe 1 hat ihren Schwerpunkt noch bei der klassischen
+      Aufgabe eines kommunalen Geoportals, einen digitalen Kartenatlas der Stadt
+      in Form eines Baukastens kombinierbarer Kartenebenen bereitzustellen.
+      Wählen Sie eine der folgenden farbigen Schaltflächen, um sich weitere
+      Bedienungs- und Hintergrundinformationen zu dem entsprechenden Thema
+      anzeigen zu lassen.{" "}
+      <span className="hide-on-small-screens">
         Als alternative Unterstützung steht Ihnen auch eine{" "}
         <Link
           onClick={() => {
@@ -24,7 +23,7 @@ const Help05Introduction = ({ showOverlayFromOutside }) => {
         </Link>{" "}
         zur Verfügung, mit der Sie gezielt die Nutzung und Funktionsweise der
         einzelnen Bedienungselemente des Portals erforschen können.
-      </p>
+      </span>
     </span>
   );
 };
