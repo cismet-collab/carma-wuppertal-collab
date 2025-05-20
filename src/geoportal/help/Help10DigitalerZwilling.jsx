c65639a77e38969f8d593917ba2d55cea67b6715
default <stefan.sander@t-online.de>
2025-05-20 14:51:13 +0000

    Hintergrund UDZ /4

--- src/geoportal/help/Help10DigitalerZwilling.jsx
+++ src/geoportal/help/Help10DigitalerZwilling.jsx
@@ -10,7 +10,19 @@ const Component = () => {
       sectionBsStyle="warning"
       sectionContent=<GenericDigitalTwinReferenceTextComponent pretext={
         <>
-          Willkommen in der Portalkomponente zum <strong>DigiTal Zwilling</strong>, dem Urbanen Digitalen Zwilling der Stadt Wuppertal!
+          Willkommen in der Portalkomponente zum <strong>DigiTal Zwilling</strong>, 
+          dem{" "}  <a href="https://smart.wuppertal.de/projekte/digital-zwilling.php">
+          Urbanen Digitalen Zwilling der Stadt Wuppertal!</a>{" "} Hier haben Sie Zugang 
+          zu allen Datenquellen des DigiTal Zwillings und allen allgemeinen 
+          Visualisierungsmethoden und Funktionen, die für eine größere Anzahl von 
+          Datenquellen und Anwendungssituationen von Nutzen sind. Zum DigiTal Zwilling 
+          gehören auch die sog. Teilzwillinge. Dies sind Anwendungen, die einen 
+          themenspezifischen Ausschnitt aus den Daten und Methoden des DigiTal Zwillings 
+          für einen spezifischen Anwendungsfall darstellen, z. B. die{" "} 
+          <a href="https://digital-twin-wuppertal-live.github.io/rainhazardmap/">Starkregengefahrenkarte</a>. 
+          In den Teilzwillingen finden Sie weitergehende, spezielle Visualisierungsmethoden 
+          und Funktionen, die wir Geoportal nicht anbieten. Zu den verfügbaren Teilzwillingen 
+          gelangen Sie über den Dialog Karteninhalte hinzufügen.
         </>
         } 
       />
