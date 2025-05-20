4aa509987ea7991823235df7b0ba8762b52994df
Thorsten Hell <thorsten.hell@cismet.de>
2025-05-20 14:12:01 +0200

    wo #6: removed TopicMapContextProvider dependency

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import data from "./secondary-info-modals/_data/luftmessstationen";
 import Sim from "./secondary-info-modals/LuftmessstationenSIM";
-import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div>
-      <TopicMapContextProvider>
+
         <div id="myMenu">
           <Sim
             feature={data["Hofkamp 86"]}
@@ -16,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             versionString="myVersion"
           />
         </div>
-      </TopicMapContextProvider>
+
     </div>
   </React.StrictMode>
 );
