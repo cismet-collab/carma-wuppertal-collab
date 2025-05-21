9c1c655acb320c601a57ccc68d392254febc0fb0
Thorsten Hell <thorsten.hell@cismet.de>
2025-05-21 15:18:55 +0200

    reset debugging main

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import data from "./secondary-info-modals/_data/luftmessstationen";
-import Sim from "./secondary-info-modals/LuftmessstationenSIM";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div>
-
-        <div id="myMenu">
-          <Sim
-            feature={data["Hofkamp 86"]}
-            setOpen={() => {}}
-            versionString="myVersion"
-          />
-        </div>
-
+      ... add here components which make problems when debugging in storybook
     </div>
   </React.StrictMode>
 );
