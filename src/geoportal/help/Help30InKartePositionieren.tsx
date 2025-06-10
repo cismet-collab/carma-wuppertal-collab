38d6bbc9193caee59a3d407119126761601a48c6
default <stefan.sander@t-online.de>
2025-06-10 15:54:51 +0000

    Textüberarbeitung Positionierung

--- src/geoportal/help/Help30InKartePositionieren.tsx
+++ src/geoportal/help/Help30InKartePositionieren.tsx
@@ -17,15 +17,15 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
             pretext={
               <>
                 <p>
-                  Sowohl in der 2D-Kartenbetrachtung wie auch im 3D-Modus können
-                  die Werkzeuge "<b>+</b>" und "<b>-</b>" (links oben in der
-                  Werkzeugleiste im Kartenfenster) zum Vergrößern bzw.
-                  Verkleinern des Ansichtsmaßstabes in festen Zoomstufen
-                  verwendet werden. Für solche Maßstabsveränderungen, zum
-                  Verschieben des betrachteten Ausschnittes sowie zum Drehen und
-                  Kippen einer 3D-Ansicht stehen darüber hinaus die in
-                  Web-Kartenanwendungen übliche Mausaktionen bzw.
-                  Touchscreen-Gesten zur Verfügung{" "}
+                  In der 2D-Ansicht und im 3D-Modus können Sie die Ansicht 
+                  mithilfe der Symbole "<b>+</b>" und "<b>-</b>" oben links 
+                  in der Werkzeugleiste in festen Zoomstufen vergrößern oder 
+                  verkleinern. Zusätzlich stehen Ihnen die in Webkarten üblichen 
+                  Steuerungsmöglichkeiten zur Verfügung: Mit der Maus (z. B. Klicken 
+                  und Ziehen, Scrollen) oder durch Touch-Gesten auf dem Bildschirm 
+                  können Sie den Kartenausschnitt verschieben oder die Ansicht 
+                  verändern (z. B. Drehen und Kippen einer 3D-Ansicht). Eine 
+                  Übersicht dieser Bedienmöglichkeiten finden Sie auf der {" "}
                   <span className="hide-on-small-screens">
                     (
                     <HelpOverlayLink
@@ -33,22 +33,19 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                         showOverlayFromOutside("CENTER");
                       }}
                     >
-                      Übersicht auf der Hilfefolie
+                      Hilfefolie
                     </HelpOverlayLink>
                     ).
                   </span>{" "}
-                  Im 3D-Modus wird zusätzlich ein Spezialwerkzeug zur Steuerung
-                  der 3D-Ansicht aktiviert, nämlich die Kompassnadel{" "}
-                  <CompassNeedle /> über dem 3D/2D-Wechselschalter. Wenn Sie das
-                  Werkzeug mit der linken Maustaste anklicken und diese gedrückt
-                  halten, können Sie durch Bewegung der Maus die Ansicht kippen
-                  und drehen (Bedienung mit einer Hand). Auf einem Touchscreen
-                  steht diese Steuerungsmöglichkeit nicht zur Verfügung. Ein
-                  einfacher Klick auf das Kompasswerkzeug richtet die
-                  Darstellung nach Norden aus. Ein Doppelklick kippt die Ansicht
-                  zusätzlich in die Senkrechtperspektive. Ein weiterer Klick
-                  kippt die Ansicht wieder in die zuletzt verwendete
-                  Bildneigung.
+                  Im 3D-Modus wird außerdem ein spezielles Steuerwerkzeug eingeblendet: 
+                  eine Kompassnadel{" "}<CompassNeedle /> oberhalb des Schalters zum 
+                  Wechsel zwischen 2D- und 3D-Ansicht. Wenn Sie auf dieses Symbol mit 
+                  der linken Maustaste klicken und diese gedrückt halten, können Sie 
+                  durch Bewegung der Maus die Ansicht kippen oder drehen – eine sogenannte 
+                  Einhandsteuerung. Auf einem Touchscreen ist diese Funktion nicht verfügbar.   
+                  Ein einfacher Klick auf die Kompassnadel richtet die Ansicht nach Norden 
+                  aus. Ein Doppelklick stellt die senkrechte Draufsicht wieder her. Ein 
+                  weiterer Klick kippt die Ansicht zurück in die zuvor verwendete Perspektive.
                 </p>
                 <p>
                   Um eine bestimmte Stelle des Stadtgebietes zu erkunden, geben
@@ -57,7 +54,7 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                   Eingabefeld links unten ein. In der inkrementellen
                   Auswahlliste werden Ihnen schon nach der Eingabe des ersten
                   Buchstabens passende Treffer angeboten. (Wenn Sie weitere
-                  Zeichen eingeben, wird der Inhalt der Auswahlliste angepasst.)
+                  Zeichen eingeben, wird der Inhalt der Auswahlliste angepasst.) 
                   Durch das vorangestellte Symbol erkennen Sie, ob es sich dabei
                   um einen <Icon name="circle" /> Stadtbezirk, ein{" "}
                   <Icon name="pie-chart" /> Quartier, eine <Icon name="home" />{" "}
