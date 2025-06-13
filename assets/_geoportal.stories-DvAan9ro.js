7d26285e09ecb9e6dc32edae246f01d18664445f
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2025-06-13 10:32:43 +0000

    deploy: 501eba36efbcf1c4085913382f33ed076ba30916

---
+++ assets/_geoportal.stories-DvAan9ro.js
@@ -0,0 +1,90 @@
+import{j as e}from"./jsx-runtime-CiKstLBL.js";import{T as K}from"./TopicMapContextProvider-Db-PvEeJ.js";import{G as D}from"./ModalApplicationMenu-AzjNk1-N.js";import{r as p}from"./index-CoXXcpNP.js";import{H as P,F as R,C as A,a as E,b as $,c as F,d as G,e as V,f as W,g as U}from"./geoportal-BzzEkkC9.js";import{D as d}from"./Section-DLjBWBCs.js";import"./TrinkwasserModal-CDa4FoFM.js";import"./_sim-component-dictionary.stories-DJOsma7r.js";import{M as _,B as L}from"./index-D4f71bbD.js";import"./ResponsiveTopicMapContextProvider-B2z_qqqc.js";import"./leaflet-src-CpR2ONvF.js";import"./setPrototypeOf-BCvhvFfc.js";import"./index-BOR0u_yr.js";import"./Modal-BczeHCXA.js";import"./InKartePositionierenText-PlCtxWuS.js";import"./MeinStandpunktMarkerMitKompass-CruLZ31C.js";import"./FuzzySearchParagraph-ClfIvOTd.js";import"./CompassNeedle-CJMkuLKM.js";import"./Table-DxK-m07a.js";import"./_emob-sim.stories-CINJNeGF.js";import"./SecondaryInfoFooter-BOVGzjH9.js";import"./ConfigurableDocBlocks-BUD-5hwq.js";import"./CustomizationContextProvider-CEe6qQic.js";import"./_ebikes-sim.stories-BC8mAMda.js";import"./SecondaryInfoFooter-DPWDGW9v.js";import"./_luftmessstationen-sim.stories-DcFt4EyA.js";import"./SecondaryInfoFooter-C9lGSM5I.js";import"./_x-and-ride-sim.stories-9fxZ5qfa.js";import"./SecondaryInfoFooter-DEOQMlfs.js";import"./_klimaorte-sim.stories-AOhuB4tl.js";import"./ContextIsolator-DIc7qPEd.js";const N=""+new URL("extServiceBackground-DhSjF3s-.jpg",import.meta.url).href,q="Kompaktanleitung und Hintergrundinformationen",J="Externer Service",Q="Wohin?",X="Hinweis",Y=`Dieses Geoportal ist derzeit ausschließlich für die Nutzung auf
+              Desktop-Computern optimiert. Bei der Verwendung eines mobilen
+              Endgeräts kann es zu Funktionseinschränkungen kommen. Wir arbeiten
+              aktiv an einer mobilen Version und danken Ihnen für Ihr
+              Verständnis.`,Z="Verstanden",l={headerText:X,bodyText:Y,confirmButtonText:Z},O=({versionString:c,showOverlayFromOutside:n=()=>{}})=>{const M=()=>{const{setAppMenuActiveMenuSection:a}=p.useContext(d);return e.jsx(R,{version:c,setAppMenuActiveMenuSection:a})},w=e.jsx(P,{showOverlayFromOutside:n}),H="info",z="Kompaktanleitung und Hintergrundinformationen",B=()=>{const{setAppMenuActiveMenuSection:a}=p.useContext(d);return[e.jsx(A,{setAppMenuActiveMenuSection:a},"zwilling"),e.jsx(E,{showOverlayFromOutside:n},"HintergrundkarteAuswaehlen"),e.jsx($,{showOverlayFromOutside:n},"InKartePositionieren"),e.jsx(F,{showOverlayFromOutside:n},"KarteninhalteHinzufuegen"),e.jsx(G,{},"KarteninhalteUntersuchen"),e.jsx(V,{},"KartenDrucken"),e.jsx(W,{showOverlayFromOutside:n},"MeinStandort"),e.jsx(U,{},"Haftungsausschluss")]};return{menuIntroduction:w,menuIcon:H,menuTitle:z,menuSections:e.jsx(B,{}),menuFooter:e.jsx(M,{})}},Be={title:"Geoportal"},t={render:()=>e.jsx(K,{appKey:"storybook-appkey",children:e.jsx(D,{...O({versionString:"xyz"}),visible:!0})})},o={render:()=>e.jsx("div",{children:Q})},r={render:()=>e.jsx("div",{children:q})},i={render:()=>e.jsx(_,{title:l.headerText,open:!0,closable:!1,closeIcon:!1,footer:[e.jsx(L,{type:"primary",children:l.confirmButtonText})],children:e.jsx("p",{children:l.bodyText})})},s={render:()=>e.jsx("div",{style:{height:"500px",width:"500px"},children:e.jsx("div",{style:{display:"flex",flexDirection:"column",cursor:"pointer",borderRadius:"0.5rem",width:"100%",boxShadow:"0 1px 2px 0 rgba(0, 0, 0, 0.05)",height:"fit-content",backgroundColor:"white"},children:e.jsx("div",{style:{position:"relative",overflow:"hidden",backgroundColor:"white",isolation:"isolate",borderRadius:"0.375rem",display:"flex",justifyContent:"center",alignItems:"center",width:"100%",aspectRatio:"1.7777/1"},children:e.jsxs("div",{style:{height:"100%",width:"100%"},children:[e.jsx("img",{src:N,alt:"",loading:"lazy",style:{objectFit:"cover",position:"relative",height:"100%",overflow:"clip",width:"calc(130% + 7.2px)",transition:"all 200ms"}}),e.jsx("div",{style:{position:"absolute",inset:0,display:"flex",alignItems:"flex-start",justifyContent:"center",paddingTop:"15%"},children:e.jsx("span",{style:{color:"black",opacity:.4,fontSize:"2.5rem",fontWeight:"bold"},children:J})})]})})})})};var m,u,x;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    const versionString = "xyz";
+    return <TopicMapContextProvider appKey="storybook-appkey">
+        <ModalApplicationMenu {...getCollabedHelpComponentConfig({
+        versionString
+      })} visible={true} />
+      </TopicMapContextProvider>;
+  }
+}`,...(x=(u=t.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};var h,g,f;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{searchTextPlaceholder}</div>;
+  }
+}`,...(f=(g=o.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var v,y,b;r.parameters={...r.parameters,docs:{...(v=r.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{tooltipText}</div>;
+  }
+}`,...(b=(y=r.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var j,S,C;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => {
+    return <Modal title={mobileInfo.headerText} open={true} closable={false} closeIcon={false} footer={[<Button type="primary">{mobileInfo.confirmButtonText}</Button>]}>
+        <p>{mobileInfo.bodyText}</p>
+      </Modal>;
+  }
+}`,...(C=(S=i.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var T,k,I;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => {
+    return <div style={{
+      height: "500px",
+      width: "500px"
+    }}>
+        <div style={{
+        display: "flex",
+        flexDirection: "column",
+        cursor: "pointer",
+        borderRadius: "0.5rem",
+        width: "100%",
+        boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+        height: "fit-content",
+        backgroundColor: "white"
+      }}>
+          <div style={{
+          position: "relative",
+          overflow: "hidden",
+          backgroundColor: "white",
+          isolation: "isolate",
+          borderRadius: "0.375rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          aspectRatio: "1.7777/1"
+        }}>
+            <div style={{
+            height: "100%",
+            width: "100%"
+          }}>
+              <img src={extServiceBackgroundImage} alt={""} loading="lazy" style={{
+              objectFit: "cover",
+              position: "relative",
+              height: "100%",
+              overflow: "clip",
+              width: "calc(130% + 7.2px)",
+              transition: "all 200ms"
+            }} />
+              <div style={{
+              position: "absolute",
+              inset: 0,
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              paddingTop: "15%"
+            }}>
+                <span style={{
+                color: "black",
+                opacity: 0.4,
+                fontSize: "2.5rem",
+                fontWeight: "bold"
+              }}>
+                  {extServiceText}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>;
+  }
+}`,...(I=(k=s.parameters)==null?void 0:k.docs)==null?void 0:I.source}}};const Ke=["KompletterModalerDialog","TextInSearchPlaceholder","TextTooltip","MobileInfo","ExtServiceStory"];export{s as ExtServiceStory,t as KompletterModalerDialog,i as MobileInfo,o as TextInSearchPlaceholder,r as TextTooltip,Ke as __namedExportsOrder,Be as default};
---
+++ assets/_x-and-ride.stories-CgrFM95a.js
@@ -0,0 +1,59 @@
+import{j as e}from"./jsx-runtime-CiKstLBL.js";import{a as n}from"./index-B-lxVbXh.js";import{T as D}from"./TopicMapContextProvider-Db-PvEeJ.js";import{I as J,M as L,K as N,F as Q,S as U}from"./SecondaryInfoFooter-DEOQMlfs.js";import{G as M}from"./Section-DLjBWBCs.js";import"./index-CoXXcpNP.js";import"./v4-CtRu48qb.js";import"./ResponsiveTopicMapContextProvider-B2z_qqqc.js";import"./leaflet-src-CpR2ONvF.js";import"./setPrototypeOf-BCvhvFfc.js";import"./index-BOR0u_yr.js";import"./ConfigurableDocBlocks-BUD-5hwq.js";import"./CustomizationContextProvider-CEe6qQic.js";import"./MeinStandpunktMarkerMitKompass-CruLZ31C.js";import"./InKartePositionierenText-PlCtxWuS.js";import"./FuzzySearchParagraph-ClfIvOTd.js";const W="P+R | B+R | Stadtteil | Adresse | POI",Z=()=>"Filter | Einstellungen | Kompaktanleitung",ee=()=>"Keine Anlagen gefunden!",ne=()=>"Filter, Einstellungen und Kompaktanleitung",te=(l,q)=>{let m;return m="Angebote",`Filter (${l} ${m} gefunden, davon ${q} in der Karte)`},re="primary",oe="Kompaktanleitung",se="default",fe={title:"X-and-ride"},t={render:()=>e.jsx("div",{children:W})},r={render:()=>e.jsx(ee,{})},o={render:()=>e.jsx(J,{setAppMenuVisible:n("setAppMenuVisible"),setAppMenuActiveMenuSection:n("setAppMenuActiveMenuSection")})},s={render:()=>e.jsx(Z,{})},i={render:()=>e.jsx(ne,{})},c={render:()=>e.jsx(D,{appKey:"storybook-appkey",children:e.jsx("div",{children:e.jsx(L,{setAppMenuActiveMenuSection:n("setAppMenuActiveMenuSection")})})})},a={render:()=>{const l={overflowY:"auto",overflowX:"hidden",maxHeight:"100vh"};return e.jsx("div",{id:"myMenu",style:l,children:e.jsx(N,{setAppMenuActiveMenuSection:n("setAppMenuActiveMenuSection")})})}},p={render:()=>e.jsx(Q,{version:"xyz",setAppMenuActiveMenuSection:n("setAppMenuActiveMenuSection")})},u={render:()=>e.jsx("div",{style:{width:"900px"},children:e.jsx(U,{close:n("close"),version:"xyz"})})},d={render:()=>e.jsxs("div",{children:[e.jsx(M,{sectionBsStyle:re,sectionTitle:te(100,10)}),e.jsx(M,{sectionBsStyle:se,sectionTitle:oe})]})};var S,x,v;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{searchTextPlaceholder}</div>;
+  }
+}`,...(v=(x=t.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var A,T,g;r.parameters={...r.parameters,docs:{...(A=r.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => {
+    return <InfoBoxTextTitle />;
+  }
+}`,...(g=(T=r.parameters)==null?void 0:T.docs)==null?void 0:g.source}}};var y,f,I;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => {
+    return <InfoBoxTextContent setAppMenuVisible={action("setAppMenuVisible")} setAppMenuActiveMenuSection={action("setAppMenuActiveMenuSection")} />;
+  }
+}`,...(I=(f=o.parameters)==null?void 0:f.docs)==null?void 0:I.source}}};var h,j,B;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuTooltip />;
+  }
+}`,...(B=(j=s.parameters)==null?void 0:j.docs)==null?void 0:B.source}}};var F,K,k;i.parameters={...i.parameters,docs:{...(F=i.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuTitle />;
+  }
+}`,...(k=(K=i.parameters)==null?void 0:K.docs)==null?void 0:k.source}}};var P,C,b;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => {
+    return <TopicMapContextProvider appKey="storybook-appkey">
+        <div>
+          <MenuIntroduction setAppMenuActiveMenuSection={action("setAppMenuActiveMenuSection")} />
+        </div>
+      </TopicMapContextProvider>;
+  }
+}`,...(b=(C=c.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};var w,z,E;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => {
+    const modalBodyStyle: CSSProperties = {
+      overflowY: "auto",
+      overflowX: "hidden",
+      maxHeight: "100vh"
+    };
+    return <div id="myMenu" style={modalBodyStyle}>
+        <KompaktanleitungSection setAppMenuActiveMenuSection={action("setAppMenuActiveMenuSection")} />
+      </div>;
+  }
+}`,...(E=(z=a.parameters)==null?void 0:z.docs)==null?void 0:E.source}}};var H,V,R;p.parameters={...p.parameters,docs:{...(H=p.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: () => {
+    return <Footer version="xyz" setAppMenuActiveMenuSection={action("setAppMenuActiveMenuSection")} />;
+  }
+}`,...(R=(V=p.parameters)==null?void 0:V.docs)==null?void 0:R.source}}};var X,$,G;u.parameters={...u.parameters,docs:{...(X=u.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: () => {
+    return <div style={{
+      width: "900px"
+    }}>
+        <SecondaryInfoFooter close={action("close")} version="xyz" />
+      </div>;
+  }
+}`,...(G=($=u.parameters)==null?void 0:$.docs)==null?void 0:G.source}}};var O,Y,_;d.parameters={...d.parameters,docs:{...(O=d.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: () => {
+    return <div>
+        <Section sectionBsStyle={FilterStyle} sectionTitle={getFilterHeader(100, 10)} />
+        <Section sectionBsStyle={KompaktAnleitungStyle} sectionTitle={KompaktAnleitungTitle} />
+      </div>;
+  }
+}`,...(_=(Y=d.parameters)==null?void 0:Y.docs)==null?void 0:_.source}}};const Ie=["TextInSearchPlaceholder","TextInInfoBoxTitle","TextInInfoBoxContent","menuTooltipString","TitleMenu","IntroductionMenu","Kompaktanleitung","MenuFooter","InfoFooter","SecionTitles"];export{u as InfoFooter,c as IntroductionMenu,a as Kompaktanleitung,p as MenuFooter,d as SecionTitles,o as TextInInfoBoxContent,r as TextInInfoBoxTitle,t as TextInSearchPlaceholder,i as TitleMenu,Ie as __namedExportsOrder,fe as default,s as menuTooltipString};
