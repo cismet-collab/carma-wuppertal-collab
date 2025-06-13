714262d1ffb7bcdd61579bec82c39afcf553c6d6
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-06-13 13:20:35 +0200

    add verdis desktop mobile warning

---
+++ src/verdis-desktop/mobileInfo.ts
@@ -0,0 +1,15 @@
+const headerText = "Hinweis";
+const bodyText = `VerDIS-Desktop ist derzeit ausschließlich für die Nutzung auf
+              Desktop-Computern optimiert. Bei der Verwendung eines mobilen
+              Endgeräts kann es zu Funktionseinschränkungen kommen. Wir arbeiten
+              aktiv an einer mobilen Version und danken Ihnen für Ihr
+              Verständnis.`;
+const confirmButtonText = "Verstanden";
+const isHardMode = true;
+
+export const mobileInfo = {
+  headerText,
+  bodyText,
+  confirmButtonText,
+  isHardMode,
+};
