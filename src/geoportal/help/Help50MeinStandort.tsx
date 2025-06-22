fb933960e29d2053ae7c1f047a82b59980006735
default <stefan.sander@t-online.de>
2025-06-22 20:20:26 +0000

    Mobile Geräte

--- src/geoportal/help/Help50MeinStandort.tsx
+++ src/geoportal/help/Help50MeinStandort.tsx
@@ -54,19 +54,21 @@ const Help40MeinStandort = ({ showOverlayFromOutside }) => {
               alt="MeinStandpunktMarker"
               src={meinStandpunktMarkerMitKompass}
             />{" "}
-            markiert wird. Das kleine blaue Dreieck am Rand dieses Standortsymbols zeigt ihre Blickrichtung an. 
-            Wenn Sie sich mit Ihrem Endgerät bewegen, wird Ihre aktuelle Position in schneller Folge neu bestimmt 
-            und stets in der Mitte der Karte angezeigt. Die Karte wird dazu automatisiert nachgeführt. Das 
-            Standortsymbol ist umgeben von einem zweiten Kreis mit transparenter, blauer Füllung, dessen Radius 
-            die Unsicherheit der Positionsbestimmung angibt{" "}
+            markiert wird. Das kleine blaue Dreieck am Rand dieses Standortsymbols zeigt ihre 
+            Blickrichtung an. Wenn Sie sich mit Ihrem Endgerät bewegen, wird Ihre aktuelle Position 
+            in schneller Folge neu bestimmt und stets in der Mitte der Karte angezeigt. Die Karte 
+            wird dazu automatisiert nachgeführt. Das Standortsymbol ist umgeben von einem zweiten 
+            Kreis mit transparenter, blauer Füllung, dessen Radius die Unsicherheit der 
+            Positionsbestimmung angibt{" "}
             <img
               alt="MeinStandpunktMarkerDoppel"
               src={meinStandpunktMarkerDoppel}
             />. 
-            Durch Antippen des inneren Kreises können Sie einen Texthinweis mit einer konkreten Angabe der aktuellen 
-            Positionsgenauigkeit einblenden. Dieser Hinweis kann durch das Antippen einer beliebigen Stelle außerhalb 
-            seines Textfeldes geschlossen werden. Ein weiteres Antippen von "Mein Standort" schaltet die Anzeige Ihrer 
-            Position wieder ab (schwarzes Werkzeug-Symbol). 
+            Durch Antippen des inneren Kreises können Sie einen Texthinweis mit einer konkreten 
+            Angabe der aktuellen Positionsgenauigkeit einblenden. Dieser Hinweis kann durch das 
+            ntippen einer beliebigen Stelle außerhalb seines Textfeldes geschlossen werden. Ein 
+            weiteres Antippen von "Mein Standort" schaltet die Anzeige Ihrer Position wieder ab 
+            (schwarzes Werkzeug-Symbol). 
           </p>
           <p>
             Wenn Sie die Karte im aktivierten &quot;Follow me&quot;-Modus
@@ -83,26 +85,7 @@ const Help40MeinStandort = ({ showOverlayFromOutside }) => {
             &quot;Follow me&quot;-Modus zurückgewechselt wird. Wenn Ihr
             aktueller Standort dagegen noch in der Karte sichtbar ist, wird
             durch diese Aktion die Anzeige der Position abgeschaltet (schwarzes
-            Werkzeug-Symbol). Sie können die Unterbrechung des &quot;Follow
-            me&quot;-Modus bei einer Wanderung o. ä. nutzen, wenn Sie sich die
-            vor Ihnen liegende Strecke in der Karte anschauen wollen.
-            Verschieben Sie dazu den Kartenausschnitt manuell auf den Abschnitt
-            Ihres Interesses. Ihr aktueller Standort wird dann zumeist nicht
-            mehr dargestellt werden können. Der &quot;Follow me&quot;-Modus wird
-            unterbrochen (blaues Werkzeug-Symbol). Wenn Sie sich ausreichend
-            über den weiteren Streckenverlauf informiert haben, reaktivieren Sie
-            durch erneutes Antippen von &quot;Mein Standort&quot; den
-            &quot;Follow me&quot;-Modus und setzen die Karte damit wieder auf
-            Ihren aktuellen Standort zurück.{" "}
-          </p>
-          <p>
-            Noch ein Hinweis zur Genauigkeit: Die Richtigkeit der
-            Positionsanzeige ist nicht garantiert. Ihre Genauigkeit hängt von
-            verschiedenen Faktoren ab, u. a. von der Qualität des
-            GNSS-Empfängers in Ihrem Endgerät, der Umgebungssituation (z. B.
-            hohe Gebäude oder spiegelnde Oberflächen in der Nachbarschaft) sowie
-            der Methode, mit der Ihr Endgerät und der von Ihnen verwendete
-            Browser die Position bestimmen.
+            Werkzeug-Symbol).
           </p>
           <p
             style={{
