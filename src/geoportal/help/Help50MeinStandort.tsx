ae1f006aa02438813bc989a7b51f38295f5cde70
default <stefan.sander@t-online.de>
2025-06-23 16:09:00 +0000

    Mobile Geräte Links /2

--- src/geoportal/help/Help50MeinStandort.tsx
+++ src/geoportal/help/Help50MeinStandort.tsx
@@ -29,14 +29,14 @@ const Help40MeinStandort = ({ showOverlayFromOutside }) => {
             der graphischen Oberfläche wie am Desktop-Rechner angeboten, allerdings müssen 
             Sie bei der Nutzung etwas öfter scrollen. Auf einem Smartphone stehen Ihnen 
             dagegen zwei Elemente nicht mehr zur Verfügung. Die Hilfefolie entfällt ersatzlos. 
-            Außerdem wird nicht mehr für jede Kartenebene ein eigenes{" "}
+            Außerdem wird nicht mehr für jede Kartenebene ein eigenes Ebenen-Steuerelement{" "}
             <span className="hide-on-small-screens">
               <HelpOverlayLink
                 onClick={() => {
                   showOverlayFromOutside("LAYERBUTTONS");
                 }}
               >
-                Ebenen-Steuerelement
+                (Hilfefolie)
               </HelpOverlayLink>
             </span>{" "}   
             angezeigt. Es wird nur noch das Hintergrund-Steuerelement ausgeprägt, über das 
