28717b765b1130efcc42e4e01c5c5800ba371609
default <stefan.sander@t-online.de>
2025-07-03 11:23:51 +0000

    Layerbuttons /4

--- src/geoportal/secondary/LayerButtonsText.tsx
+++ src/geoportal/secondary/LayerButtonsText.tsx
@@ -1,5 +1,5 @@
 import { HelpOverlayLink } from "../../helper-overlay/components/Link";
-import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const LayerButtonsText = () => {
   return (
