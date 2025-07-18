ffac503d318b823501d37f7806b6969eca504e71
default <stefan.sander@t-online.de>
2025-07-18 16:30:44 +0000

    Secondary-Element Homebutton

--- src/geoportal/secondary/HomeText.tsx
+++ src/geoportal/secondary/HomeText.tsx
@@ -5,8 +5,8 @@ export const HomeText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        <b>Klicken für Rückkehr zum Ausgangspunkt (2D und 3D)</b><br/> 
-        feste  Voreinstellung: Rathaus Wuppertal-Barmen | im 3D-Modus als Schrägansicht  
+        <b>Klicken für Positionierung auf Rathaus Barmen (2D und 3D)</b><br/> 
+        fester Bezugspunkt bei Verlust der Orientierung | im 3D-Modus als Schrägansicht  
       </div>
     </ul>
   );
