c46343c6aa23dc9b7c3a43e0ce43957b4fe6b9de
default <stefan.sander@t-online.de>
2025-09-03 22:31:05 +0000

    Abschnitt Karten Drucken /3

--- src/geoportal/help/Help45KartenDrucken.tsx
+++ src/geoportal/help/Help45KartenDrucken.tsx
@@ -1,5 +1,6 @@
 import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
 import { HelpOverlayLink } from "../../helper-overlay/components/Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Help45KartenDrucken = ({
   showOverlayFromOutside,
@@ -25,10 +26,12 @@ const Help45KartenDrucken = ({
             DigiTal Zwilling festhalten oder mit anderen teilen können.
           </p>
           <p>
-            Wenn Sie dort auf das Symbol <b>Karte speichern</b> klicken, öffnet sich ein kleiner Dialog zum Speichern 
-            der aktuell zusammengestellten Kartenebenen unter einem frei von Ihnen wählbaren Titel. Wenn Sie möchten, 
-            können Sie sich im Eingabefeld "<b>Inhalt</b>" notieren, um welche Kartenebenen es sich handelt. Die Karte 
-            wird im Cache-Speicher Ihres Browsers abgelegt.
+            Wenn Sie dort auf das Symbol{" "}<FontAwesomeIcon icon="file-export" />{" "}<b>Karte speichern</b> klicken, 
+            öffnet sich ein kleiner Dialog zum Speichern der aktuell zusammengestellten Kartenebenen unter einem frei 
+            von Ihnen wählbaren Titel. Wenn Sie möchten, können Sie sich im Eingabefeld "<b>Inhalt</b>" notieren, um 
+            welche Kartenebenen es sich handelt. Die Karte wird im Cache-Speicher Ihres Browsers abgelegt. Die Funktion 
+            ist daher zzt. eher zum kurzfristigen Sichern eines Bearbeitungsstandes gedacht als zum langfristigen Aufbau 
+            einer Kartensammlung.
           </p>
         </div>
       }
