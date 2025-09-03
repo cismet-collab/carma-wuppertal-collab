2bffbe73a1ba334333d3968fc3f2d3917f195766
Friedrich Hartmann <friedrich.hartmann@cismet.de>
2025-09-03 11:24:24 +0200

    chore(srgk): rename Help90HaftungsausschlussWupp.jsx -> .tsx (preserve history)

--- src/starkregengefahrenkarte/help/Help90HaftungsausschlussWupp.tsx
+++ src/starkregengefahrenkarte/help/Help90HaftungsausschlussWupp.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UIDispatchContext } from "react-cismap/contexts/UIContextProvider";
 import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-export interface Help90HaftungsausschlussWuppProps {}
-
-const Component: React.FC<Help90HaftungsausschlussWuppProps> = () => {
+const Component = ({ appName }) => {
+  const { setAppMenuActiveMenuSection } = useContext(UIDispatchContext);
 
   return (
     <GenericModalMenuSection
@@ -13,7 +13,19 @@ const Component: React.FC<Help90HaftungsausschlussWuppProps> = () => {
       sectionBsStyle='danger'
       sectionContent={
         <div>
-          {/* Content omitted for brevity; using same content as JSX version */}
+          <p>
+            Alle hier veröffentlichten Informationen wurden mit der bestmöglichen Sorgfalt
+            zusammengestellt und kontrolliert. Dennoch sind Fehler nie ausgeschlossen, sodass für
+            die Richtigkeit und damit auch Nutzbarkeit der Informationen keine Gewähr übernommen
+            werden kann (vgl. auch Abschnitt{" "}
+            <a class='renderAsLink' onClick={() => setAppMenuActiveMenuSection("aussagekraft")}>
+              Aussagekraft der Simulationen
+            </a>{" "}
+            zu den beim heutigen Stand der Technik unvermeidbaren Unschärfen und Fehlern von
+            modellbasierten Simulationen des Starkregenabflusses). Eine Haftung für Schäden, die
+            sich aus der Nutzung der Informationen ergeben, wird daher ausgeschlossen. Dem stimmen
+            Sie mit der Nutzung der Daten zu.
+          </p>
         </div>
       }
     />
