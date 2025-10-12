7bb916bea176ee9191aac3ecc40260348b103485
default <stefan.sander@t-online.de>
2025-10-12 18:46:43 +0000

    In Karte positionieren indiv. Absätze

--- src/geoportal/help/Help30InKartePositionieren.tsx
+++ src/geoportal/help/Help30InKartePositionieren.tsx
@@ -21,10 +21,10 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                   mithilfe der Symbole "<b>+</b>" und "<b>-</b>" oben links 
                   in der Werkzeugleiste in festen Zoomstufen vergrößern oder 
                   verkleinern. Zusätzlich stehen Ihnen die in Webkarten üblichen 
-                  Steuerungsmöglichkeiten zur Verfügung: Mit der Maus (z. B. Klicken 
-                  und Ziehen, Scrollen) oder durch Touch-Gesten auf dem Bildschirm 
-                  können Sie den Kartenausschnitt verschieben oder die Ansicht 
-                  verändern, z. B. eine 3D-Ansicht drehen und kippen
+                  Steuerungsmöglichkeiten zur Verfügung: Mit der Maus (z. B. 
+                  Klicken und Ziehen, Scrollen) oder durch Touch-Gesten auf 
+                  dem Bildschirm können Sie den Kartenausschnitt verschieben oder 
+                  die Ansicht verändern.
                   <span className="hide-on-small-screens">
                     {" "}
                     <HelpOverlayLink
@@ -34,9 +34,11 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                     >
                       (s. Hilfefolie)
                     </HelpOverlayLink>
-                  </span>.{" "}
+                  </span>.
+                </p>
+                <p>  
                   Im 3D-Modus wird außerdem ein spezielles Steuerwerkzeug eingeblendet: 
-                  eine Kompassnadel{" "}<CompassNeedle /> oberhalb des Schalters zum 
+                  eine Kompassnadel{" "}<CompassNeedle />{" "}oberhalb des Schalters zum 
                   Wechsel zwischen 2D- und 3D-Ansicht. Wenn Sie auf dieses Symbol mit 
                   der linken Maustaste klicken und diese gedrückt halten, können Sie 
                   durch Bewegung der Maus die Ansicht kippen oder drehen – eine sogenannte 
@@ -46,22 +48,18 @@ const Help30InKartePositionieren = ({ showOverlayFromOutside }) => {
                   weiterer Klick kippt die Ansicht zurück in die zuvor verwendete Perspektive.
                 </p>
                 <p>
-                  Um eine bestimmte Stelle des Stadtgebietes zu erkunden, geben
-                  Sie den Namen eines Stadtteils (Stadtbezirk oder Quartier),
-                  einer Adresse, eines Straßennamens oder eines POI im
-                  Eingabefeld links unten ein. In der inkrementellen
-                  Auswahlliste werden Ihnen schon nach der Eingabe des ersten
-                  Buchstabens passende Treffer angeboten. (Wenn Sie weitere
-                  Zeichen eingeben, wird der Inhalt der Auswahlliste angepasst.) 
-                  Durch das vorangestellte Symbol erkennen Sie, ob es sich dabei
-                  um einen <Icon name="circle" /> Stadtbezirk, ein{" "}
-                  <Icon name="pie-chart" /> Quartier, eine <Icon name="home" />{" "}
-                  Adresse, eine <Icon name="road" /> Straße ohne zugeordnete
-                  Hausnummern, einen <Icon name="tag" /> Interessanten Ort
-                  ("Point of Interest", kurz POI), die <Icon name="tags" />{" "}
-                  alternative Bezeichnung eines POI, eine <Icon name="child" />{" "}
-                  Kindertageseinrichtung oder eine{" "}
-                  <Icon name="graduation-cap" /> Schule handelt.
+                  Um gezielt nach einem Ort innerhalb des Stadtgebiets zu suchen, 
+                  geben Sie dessen Namen in das Eingabefeld "Wohin?" unten links ein. 
+                  Sobald Sie ein Zeichen eingegeben haben, erscheint eine Auswahlliste 
+                  mit passenden Treffern. Die Trefferliste ändert sich und wird genauer, 
+                  wenn Sie weitere Zeichen eingeben. Ein Symbol vor jedem Treffer zeigt an, 
+                  um welche Art von Ort es sich handelt: einen <Icon name="circle" /> Stadtbezirk, 
+                  ein{" "}<Icon name="pie-chart" /> Quartier, eine <Icon name="home" />{" "}
+                  Adresse, eine <Icon name="road" /> Straße ohne Hausnummern, 
+                  einen <Icon name="tag" /> Interessanten Ort ("Point of Interest", kurz POI), 
+                  eine <Icon name="tags" />{" "} alternative POI-Bezeichnung, 
+                  eine <Icon name="child" />{" "} Kindertageseinrichtung oder eine{" "}
+                  <Icon name="graduation-cap" /> Schule.
                 </p>
                 <FuzzySearchParagraph />
               </>
