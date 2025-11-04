95b86ca8e3406dfd49b55c1630fcc48fc51ed909
helllth <helllth@users.noreply.github.com>
2025-11-04 13:00:32 +0000

    deploy: 677989d965ac4298a73cb437fb067cf424a1b957

---
+++ assets/_geoportal.stories-mm7M30Dh.js
@@ -0,0 +1,93 @@
+import{j as e}from"./jsx-runtime-CiKstLBL.js";import{T as A}from"./TopicMapContextProvider-D9ezyiNu.js";import{G as R}from"./ModalApplicationMenu-7b7crVCh.js";import{r as d}from"./index-CoXXcpNP.js";import{H as E,a as V,C as G,b as W,c as U,d as $,e as _,f as N,g as q,h as J}from"./DemoGif-Comz2Ry8.js";import{D as x}from"./Section-sUH94gHW.js";import{F as Q,k as X}from"./MeinStandpunktMarkerMitKompass-DkOKJyrr.js";import{B as K,M as Y}from"./index-ssFdEkgH.js";import"./TrinkwasserModal-BXXSQWoL.js";import"./_sim-component-dictionary.stories-a5TC3-ia.js";import"./_ebikes-sim.stories-DYfr-8fm.js";import"./_emob-sim.stories-BZs-HBdo.js";import"./leaflet-src-CpR2ONvF.js";import"./ResponsiveTopicMapContextProvider-ByNYSwTA.js";import"./setPrototypeOf-CkMXiYaH.js";import"./index-BL0hGrs8.js";import"./index-BOR0u_yr.js";import"./Modal-B-vwfRoB.js";import"./InKartePositionierenText-BPN8w9V_.js";import"./FuzzySearchParagraph-NC9kWKjC.js";import"./CompassNeedle-CJMkuLKM.js";import"./useClosable-B84RqC7C.js";import"./ContextIsolator-k_ziJtNu.js";import"./genericSecondaryInfoFooterFactory-DnwWCAjj.js";import"./_luftmessstationen-sim.stories-HyyV2RrP.js";import"./index-C2sH1ddT.js";import"./_vorhabenkarte-sim.stories-Bwoo0w_U.js";import"./_x-and-ride-sim.stories-DZf1cshX.js";import"./_klimaorte-sim.stories-DX8L92ZD.js";import"./Table-DYTeeoPs.js";const Z=""+new URL("extServiceBackground-DhSjF3s-.jpg",import.meta.url).href,O=({versionString:t,showOverlayFromOutside:n=()=>{},loginFormToggle:m=()=>{},isLoginFormVisible:z=!1,loginForm:c,loginFormTrigger:u})=>{const B=()=>{const{setAppMenuActiveMenuSection:o}=d.useContext(x);return e.jsx(V,{version:t,setAppMenuActiveMenuSection:o})},D=e.jsx(E,{showOverlayFromOutside:n,isLoginFormVisible:z,loginForm:c}),F="info",L=e.jsxs("div",{style:{display:"flex",alignItems:"center",justifyContent:"space-between",width:"100%"},children:[e.jsx("span",{children:"Kompaktanleitung und Hintergrundinformationen"}),c&&u?u:c&&e.jsx(K,{type:"text",onClick:m,children:e.jsx(Q,{icon:X,size:"lg"})})]}),P=()=>{const{setAppMenuActiveMenuSection:o}=d.useContext(x);return[e.jsx(G,{setAppMenuActiveMenuSection:o},"zwilling"),e.jsx(W,{showOverlayFromOutside:n},"HintergrundkarteAuswaehlen"),e.jsx(U,{showOverlayFromOutside:n},"InKartePositionieren"),e.jsx($,{showOverlayFromOutside:n},"KarteninhalteHinzufuegen"),e.jsx(_,{showOverlayFromOutside:n,setAppMenuActiveMenuSection:o},"KarteninhalteUntersuchen"),e.jsx(N,{showOverlayFromOutside:n},"KarteninhalteDrucken"),e.jsx(q,{showOverlayFromOutside:n},"MeinStandort"),e.jsx(J,{},"Haftungsausschluss")]};return{menuIntroduction:D,menuIcon:F,menuTitle:L,menuSections:e.jsx(P,{}),menuFooter:e.jsx(B,{}),modalTitleStyle:{display:"flex",width:"100%",alignItems:"center",gap:"4px"}}},ee="Kompaktanleitung und Hintergrundinformationen",ne="Externer Service",te="Wohin?",oe="Hinweis",re=`Dieses Geoportal ist derzeit ausschließlich für die Nutzung auf
+              Desktop-Computern optimiert. Bei der Verwendung eines mobilen
+              Endgeräts kann es zu Funktionseinschränkungen kommen. Wir arbeiten
+              aktiv an einer mobilen Version und danken Ihnen für Ihr
+              Verständnis.`,ie="Verstanden",p={headerText:oe,bodyText:re,confirmButtonText:ie},Pe={title:"Geoportal"},r={render:()=>{const[t,n]=d.useState(!1);return e.jsx(A,{appKey:"storybook-appkey",children:e.jsx(R,{...O({versionString:"xyz",loginFormToggle:()=>n(!t),isLoginFormVisible:t}),visible:!0})})}},i={render:()=>e.jsx("div",{children:te})},s={render:()=>e.jsx("div",{children:ee})},a={render:()=>e.jsx(Y,{title:p.headerText,open:!0,closable:!1,closeIcon:!1,footer:[e.jsx(K,{type:"primary",children:p.confirmButtonText})],children:e.jsx("p",{children:p.bodyText})})},l={render:()=>e.jsx("div",{style:{height:"500px",width:"500px"},children:e.jsx("div",{style:{display:"flex",flexDirection:"column",cursor:"pointer",borderRadius:"0.5rem",width:"100%",boxShadow:"0 1px 2px 0 rgba(0, 0, 0, 0.05)",height:"fit-content",backgroundColor:"white"},children:e.jsx("div",{style:{position:"relative",overflow:"hidden",backgroundColor:"white",isolation:"isolate",borderRadius:"0.375rem",display:"flex",justifyContent:"center",alignItems:"center",width:"100%",aspectRatio:"1.7777/1"},children:e.jsxs("div",{style:{height:"100%",width:"100%"},children:[e.jsx("img",{src:Z,alt:"",loading:"lazy",style:{objectFit:"cover",position:"relative",height:"100%",overflow:"clip",width:"calc(130% + 7.2px)",transition:"all 200ms"}}),e.jsx("div",{style:{position:"absolute",inset:0,display:"flex",alignItems:"flex-start",justifyContent:"center",paddingTop:"15%"},children:e.jsx("span",{style:{color:"black",opacity:.4,fontSize:"2.5rem",fontWeight:"bold"},children:ne})})]})})})})};var h,g,f;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => {
+    const [showLoginForm, setShowLoginForm] = useState(false);
+    const versionString = "xyz";
+    return <TopicMapContextProvider appKey="storybook-appkey">
+        <ModalApplicationMenu {...getCollabedHelpComponentConfig({
+        versionString,
+        loginFormToggle: () => setShowLoginForm(!showLoginForm),
+        isLoginFormVisible: showLoginForm
+      })} visible={true} />
+      </TopicMapContextProvider>;
+  }
+}`,...(f=(g=r.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var v,y,b;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{searchTextPlaceholder}</div>;
+  }
+}`,...(b=(y=i.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var j,S,w;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => {
+    return <div>{tooltipText}</div>;
+  }
+}`,...(w=(S=s.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};var T,C,k;a.parameters={...a.parameters,docs:{...(T=a.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => {
+    return <Modal title={mobileInfo.headerText} open={true} closable={false} closeIcon={false} footer={[<Button type="primary">{mobileInfo.confirmButtonText}</Button>]}>
+        <p>{mobileInfo.bodyText}</p>
+      </Modal>;
+  }
+}`,...(k=(C=a.parameters)==null?void 0:C.docs)==null?void 0:k.source}}};var I,M,H;l.parameters={...l.parameters,docs:{...(I=l.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => {
+    return <div style={{
+      height: "500px",
+      width: "500px"
+    }}>
+        <div style={{
+        display: "flex",
+        flexDirection: "column",
+        cursor: "pointer",
+        borderRadius: "0.5rem",
+        width: "100%",
+        boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+        height: "fit-content",
+        backgroundColor: "white"
+      }}>
+          <div style={{
+          position: "relative",
+          overflow: "hidden",
+          backgroundColor: "white",
+          isolation: "isolate",
+          borderRadius: "0.375rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          aspectRatio: "1.7777/1"
+        }}>
+            <div style={{
+            height: "100%",
+            width: "100%"
+          }}>
+              <img src={extServiceBackgroundImage} alt={""} loading="lazy" style={{
+              objectFit: "cover",
+              position: "relative",
+              height: "100%",
+              overflow: "clip",
+              width: "calc(130% + 7.2px)",
+              transition: "all 200ms"
+            }} />
+              <div style={{
+              position: "absolute",
+              inset: 0,
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              paddingTop: "15%"
+            }}>
+                <span style={{
+                color: "black",
+                opacity: 0.4,
+                fontSize: "2.5rem",
+                fontWeight: "bold"
+              }}>
+                  {extServiceText}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>;
+  }
+}`,...(H=(M=l.parameters)==null?void 0:M.docs)==null?void 0:H.source}}};const Ae=["KompletterModalerDialog","TextInSearchPlaceholder","TextTooltip","MobileInfo","ExtServiceStory"];export{l as ExtServiceStory,r as KompletterModalerDialog,a as MobileInfo,i as TextInSearchPlaceholder,s as TextTooltip,Ae as __namedExportsOrder,Pe as default};
