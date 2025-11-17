a9d9218347cbaefbb9e587e35a8a412425f079c3
d4v3000 <d4v3000@users.noreply.github.com>
2025-11-17 13:35:33 +0000

    deploy: ddee20a186d4977bd5c2a218feaac1b1ba5b2dbe

---
+++ assets/imageUrlManipulation-DVIo1G1U.js
@@ -0,0 +1,1 @@
+const t="https://wupp-digitaltwin-assets.cismet.de/v2/geoportal/",p=e=>e&&e.replace("https://geo.wuppertal.de/geoportal/",t).replace("https://www.wuppertal.de/geoportal/",t);export{p as u};
