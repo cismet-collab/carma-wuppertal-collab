3f864bb214bd16c5ae00db0f02655106fc799612
default <stefan.sander@t-online.de>
2025-12-16 19:29:45 +0000

    Secondary Text Oblique

--- src/geoportal/secondary/Oblique.tsx
+++ src/geoportal/secondary/Oblique.tsx
@@ -5,7 +5,12 @@ export const ObliqueText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        <b>Wechselschalter zum Ein-/Ausschalten des Schrägluftbildmodus (3D)</b>
+        <b>Wechselschalter zum Ein-/Ausschalten des Schrägluftbildmodus (3D)</b><br/>
+        Fußabdruck des für die Mitte des eingestellten Kartenausschnitts besten 
+        aktuellen Schrägluftbilds  wird markiert | Anzeige spezieller Steuerelemente | 
+        "Flug zum Bild" wechselt in die Aufnahmeperspektive für unverzerrte Bildvorschau | 
+        Bild im Browser öffnen oder herunterladen (ca. 4,7 MB) | Pfeiltasten zur Drehung 
+        oder für den Wechsel zum benachbarten Bild (Fußabdruck oder Vorschau) 
       </div>
     </ul>
   );
