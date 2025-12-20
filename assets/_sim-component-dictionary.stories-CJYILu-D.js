0b1c34955c2324f35b55ec6c91113b4e57751e5d
helllth <helllth@users.noreply.github.com>
2025-12-20 09:26:53 +0000

    deploy: eb94c4de99df99577dfed4560d36ea4c4d951385

---
+++ assets/_sim-component-dictionary.stories-CJYILu-D.js
@@ -0,0 +1,23 @@
+import{m as S,S as n,_ as f,a as g}from"./_alkis-sim.stories-1oNunU7z.js";import{m as I,S as p,_ as j,a as h}from"./_emob-sim.stories-Cjm0DLj2.js";import{m as k,S as c,_ as M,a as u}from"./_ebikes-sim.stories-BifakcNK.js";import{m as v,S as b,_ as E,a as T}from"./_luftmessstationen-sim.stories-DTBJnf8s.js";import{m as $,S as l,_ as A,a as L}from"./_vorhabenkarte-sim.stories-DEKDSVZL.js";import{m as P,S as d,_ as D,a as V}from"./_x-and-ride-sim.stories-C7h3_jzS.js";import{_ as O}from"./_klimaorte-sim.stories-POnEfrLx.js";import{j as s}from"./jsx-runtime-CCjYJYRa.js";import"./index-uubelm5h.js";import"./Section-Ki2X_jEe.js";import"./index-D3eZ-H7s.js";import"./index-DcQjGrIY.js";import"./setPrototypeOf-Bdmw9Av6.js";import"./index-B-zHMOSD.js";import"./MeinStandpunktMarkerMitKompass-BL4t3f7F.js";import"./InKartePositionierenText-DM7IygXx.js";import"./genericSecondaryInfoFooterFactory-DtHSfYIs.js";import"./Modal-6Xgu3dd9.js";import"./imageUrlManipulation-DVIo1G1U.js";import"./Table-CuV1HSiQ.js";import"./ResponsiveTopicMapContextProvider-DtfXjz9-.js";import"./index-CfSZYJ7a.js";import"./TopicMapContextProvider-D1C-gWp_.js";import"./leaflet-src-ODJ4-auN.js";import"./index-BXJSkr7J.js";const C=Math.PI,K=-C,R={title:"SecondaryInfo Modals"},r=()=>{const t=Object.keys(y);return s.jsxs("div",{children:[s.jsx("h3",{children:"SIMComponentDictionary Keys"}),s.jsx("pre",{children:t.map(e=>s.jsx("li",{children:e},e))}),s.jsx("br",{}),s.jsx("br",{}),s.jsx("br",{}),s.jsx("br",{}),s.jsx("h3",{children:"Test @carma imports"}),s.jsxs("pre",{children:['import {PI_VALUES} from "@carma/math";',s.jsx("br",{}),s.jsx("br",{}),"# Example:",s.jsx("br",{}),"-pi: ",K]})]})};d.argTypes=P.argTypes;c.argTypes=k.argTypes;p.argTypes=I.argTypes;b.argTypes=v.argTypes;l.argTypes=$.argTypes;n.argTypes=S.argTypes;r.__docgenInfo={description:"",methods:[],displayName:"SIMDictionaryKeys"};var a,i,_;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const keys = Object.keys(SIMComponentDictionary);
+  return <div>
+      <h3>SIMComponentDictionary Keys</h3>
+      <pre>
+        {keys.map(key => <li key={key}>{key}</li>)}
+      </pre>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h3>Test @carma imports</h3>
+
+      <pre>
+        {'import {PI_VALUES} from "@carma/math";'}
+        <br />
+        <br />
+        # Example:
+        <br />
+        -pi: {PI_VALUES.MINUS_PI}
+      </pre>
+    </div>;
+}`,...(_=(i=r.parameters)==null?void 0:i.docs)==null?void 0:_.source}}};const U=["SIMDictionaryKeys","XAndRide","Ebikes","EMob","Luftmessstationen","Vorhabenkarte","Alkis"],X=Object.freeze(Object.defineProperty({__proto__:null,Alkis:n,EMob:p,Ebikes:c,Luftmessstationen:b,SIMDictionaryKeys:r,Vorhabenkarte:l,XAndRide:d,__namedExportsOrder:U,default:R},Symbol.toStringTag,{value:"Module"})),o=Object.assign({"./AlkisSIM.tsx":g,"./EMobSIM.jsx":h,"./EbikesSIM.tsx":u,"./LuftmessstationenSIM.tsx":T,"./VorhabenkarteSIM.tsx":L,"./XandRideSIM.tsx":V,"./_alkis-sim.stories.tsx":f,"./_ebikes-sim.stories.tsx":M,"./_emob-sim.stories.tsx":j,"./_klimaorte-sim.stories.tsx":O,"./_luftmessstationen-sim.stories.tsx":E,"./_sim-component-dictionary.stories.tsx":X,"./_vorhabenkarte-sim.stories.tsx":A,"./_x-and-ride-sim.stories.tsx":D}),y={};var m;for(const t in o){if(/index\.(tsx|jsx|js)$/.test(t)||/\.stories\.(tsx|jsx|js)$/.test(t)||/\.data\.(tsx|jsx|js)$/.test(t))continue;const e=t.match(/\.\/(.*)\.(tsx|jsx|js)$/);if(e&&((m=o[t])!=null&&m.default)){const x=e[1].charAt(0).toLowerCase()+e[1].slice(1);y[x]=o[t].default}}export{n as Alkis,p as EMob,c as Ebikes,b as Luftmessstationen,r as SIMDictionaryKeys,l as Vorhabenkarte,d as XAndRide,U as __namedExportsOrder,R as default};
