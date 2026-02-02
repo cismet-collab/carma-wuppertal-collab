45a3ba2a9b682d0f9cc293a0a32555bd455d6659
helllth <helllth@users.noreply.github.com>
2026-02-02 16:20:09 +0000

    deploy: 2dc0df41c39a0562e913a71aa07bdaabe71b0961

---
+++ assets/_verdis-desktop.stories-BMqrIXg1.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-CCjYJYRa.js";import{M as i,B as a}from"./index-By475-UW.js";import"./index-uubelm5h.js";import"./index-CiIiCbtB.js";import"./setPrototypeOf-Bdmw9Av6.js";import"./index-DcQjGrIY.js";import"./ContextIsolator-D-5BuTEN.js";const d="Hinweis",l=`VerDIS-Desktop kann auf diesem Bildschirm nicht ausgeführt werden.
+Bitte verwenden Sie ein Gerät mit größerem Display, um fortzufahren.`,m="Ok",c=!0,e={headerText:d,bodyText:l,confirmButtonText:m,isHardMode:c},B={title:"Verdis-desktop"},o={render:()=>t.jsx(i,{title:e.headerText,open:!0,closable:!1,closeIcon:!1,footer:[t.jsx(a,{disabled:e.isHardMode,type:"primary",children:e.confirmButtonText})],children:t.jsx("p",{children:e.bodyText})})};var r,n,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => {
+    return <Modal title={mobileInfo.headerText} open={true} closable={false} closeIcon={false} footer={[<Button disabled={mobileInfo.isHardMode} type="primary">
+            {mobileInfo.confirmButtonText}
+          </Button>]}>
+        <p>{mobileInfo.bodyText}</p>
+      </Modal>;
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const M=["MobileInfo"];export{o as MobileInfo,M as __namedExportsOrder,B as default};
