e3b9c976240106f2a247a69656d10f0c26f9db5a
default <stefan.sander@t-online.de>
2026-03-11 19:37:48 +0000

    Hilfe-Overlay Gazetteer-Erweiterung

--- src/geoportal/secondary/GazetteerText.tsx
+++ src/geoportal/secondary/GazetteerText.tsx
@@ -8,14 +8,17 @@ export const GazetteerText = () => {
     >
       <div>
         <b>
-          2-stufiges Eingabeelement für Positionierung über Adresse oder andere
-          Ortsbezeichnung (2D und 3D)
+          2-stufiges Eingabeelement für Positionierung über Ortsbezeichnungen, 
+          umschaltbar zwischen &quot;Adressen und Orte&quot; und &quot;Flurstücke&quot; (2D und 3D)
         </b>
-        <br /> Text eingeben für unscharfe Suche im Verzeichnis der Ortsnamen |
-        Treffer werden nach Grad der Übereinstimmung mit Suchtext klassifiziert
-        und gruppiert | Eintrag in Trefferliste anklicken zur Zentrierung der
-        Karte auf diesen Ort | Ortsmarkierung in Karte durch Placemark-Symbol
-        oder (bei Flächen) Spotlight-Effekt
+        <br /> 
+            bei &quot;Adressen und Orte&quot; (voreingestellter Modus) Text eingeben für unscharfe Suche 
+            im Verzeichnis der Ortsnamen | Treffer werden nach Grad der Übereinstimmung mit Suchtext 
+            klassifiziert und gruppiert | Eintrag in Trefferliste anklicken zur Zentrierung der Karte 
+            auf diesen Ort | Ortsmarkierung in Karte durch Placemark-Symbol oder (bei Flächen) 
+            Spotlight-Effekt | bei &quot;Flurstücke&quot; nacheinander Eintrag aus Vorschlagsliste auswählen 
+            für Bestandteile Gemarkung, Flur und Flurstücksnummer des Flurstückskennzeichens, Suchtexte können 
+            zur Verkürzung der Listen eingegeben werden (unscharfe Suche hier nur beim Gemarkungsnamen)
         <br />
         <HelpOverlayLink section="positionieren">
           weitere Informationen
