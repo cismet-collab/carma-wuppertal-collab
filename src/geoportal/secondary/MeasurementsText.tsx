9b2ad0340f189c48c6e9f316b02472279b2bee04
default <stefan.sander@t-online.de>
2026-06-02 18:33:51 +0000

    Messungsmodus Erweiterung /1

--- src/geoportal/secondary/MeasurementsText.tsx
+++ src/geoportal/secondary/MeasurementsText.tsx
@@ -7,16 +7,16 @@ export const MeasurementsText = () => {
       style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
     >
       <div>
-        <b>Wechselschalter zum Ein-/Ausschalten des Messungsmodus (2D)</b>
+        <b>Wechselschalter zum Ein-/Ausschalten des Messungsmodus</b>
         <br />
-        <HelpOverlayLink target="LAYERBUTTONS">Zeichnung-Steuerelement</HelpOverlayLink> 
+        <HelpOverlayLink target="LAYERBUTTONS">Zeichnung-Steuerelement</HelpOverlayLink>{" "} 
         zur Arbeit mit allen im Browser gespeicherten 
         Messungsgeometrien (Zoom, Speichern, Löschen) sowie in 3D zur Auswahl 
         spezieller Messmodi | in Karte klicken zum Zeichnen von 
         Messgeometrien | Stützpunkte von Vektorlayern werden eingefangen 
         ("Snapping") | Linienzüge beenden mit Doppelklick, Flächen schließen 
         durch erneutes Anklicken des Startpunktes | Anzeige einer einzelnen 
-        selektierten Messung und Durchwandern aller Messungen in der 
+        selektierten Messung und Durchwandern aller Messungen in der{" "} 
         <HelpOverlayLink target="INFOBOX">Info-Box</HelpOverlayLink> | Speichern
         über Zeichnung-Steuerelement als GeoJSON-Datei oder (nur 2D) im Geoportal
         unter Objekte/Meine Messungen  
