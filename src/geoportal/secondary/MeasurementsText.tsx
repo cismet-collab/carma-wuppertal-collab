5540b0d7fe92a494230f4125ecd01097eed77006
default <stefan.sander@t-online.de>
2026-06-02 16:39:38 +0000

    Messunngsmodus Erweiterung

--- src/geoportal/secondary/MeasurementsText.tsx
+++ src/geoportal/secondary/MeasurementsText.tsx
@@ -1,7 +1,3 @@
-import stepOne from "./assets/m-step-1.png";
-import stepTwo from "./assets/m-step-2.png";
-import stepThree from "./assets/m-step-3.png";
-import stepFour from "./assets/m-step-4.png";
 import { HelpOverlayLink } from "../../helper-overlay/components/Link";
 
 export const MeasurementsText = () => {
@@ -13,27 +9,17 @@ export const MeasurementsText = () => {
       <div>
         <b>Wechselschalter zum Ein-/Ausschalten des Messungsmodus (2D)</b>
         <br />
-        in Karte klicken zum Zeichnen von Messgeometrien | Stützpunkte von 
-        Vektorlayern werden eingefangen ("Snapping") | Linienzüge beenden
-        mit Doppelklick, Flächen schließen durch erneutes Anklicken des
-        Startpunktes | Ergebnisanzeige und Verwalten mehrerer Messgeometrien in
-        der <HelpOverlayLink target="INFOBOX">Info-Box</HelpOverlayLink> |
-        Speicherung der Messgeometrien im Browser
-      </div>
-      <div>
-        <div
-          style={{
-            marginTop: "10px",
-            marginLeft: "-10px",
-            display: "flex",
-            gap: "10px",
-          }}
-        >
-          <img src={stepOne} style={{ height: "120px" }} />
-          <img src={stepTwo} style={{ height: "120px" }} />
-          <img src={stepThree} style={{ height: "120px" }} />
-          <img src={stepFour} style={{ height: "120px" }} />
-        </div>
+        <HelpOverlayLink target="LAYERBUTTONS">Zeichnung-Steuerelement</HelpOverlayLink> 
+        zur Arbeit mit allen im Browser gespeicherten 
+        Messungsgeometrien (Zoom, Speichern, Löschen) sowie in 3D zur Auswahl 
+        spezieller Messmodi | in Karte klicken zum Zeichnen von 
+        Messgeometrien | Stützpunkte von Vektorlayern werden eingefangen 
+        ("Snapping") | Linienzüge beenden mit Doppelklick, Flächen schließen 
+        durch erneutes Anklicken des Startpunktes | Anzeige einer einzelnen 
+        selektierten Messung und Durchwandern aller Messungen in der 
+        <HelpOverlayLink target="INFOBOX">Info-Box</HelpOverlayLink> | Speichern
+        über Zeichnung-Steuerelement als GeoJSON-Datei oder (nur 2D) im Geoportal
+        unter Objekte/Meine Messungen  
       </div>
     </ul>
   );
